2f314624758b5eaab9f4ee67e6192a4dec15d4a4
opacity <2249617065@qq.com>
2024-04-01 16:56:12 +0800

    feat(special-column)：专栏，专栏详情页，专栏答题页

---
+++ src/app/special-column-answer/page.tsx
@@ -0,0 +1,73 @@
+import Image from "next/image";
+
+const Page = () => {
+    return <div className={"relative bg-#F5F7FB h-screen"}>
+        <div className={"ml-16px mb-50px"}>
+            {/*上方分享*/}
+            <div className={"flex justify-end items-center pt-16px"}>
+                <div
+                    className={"inline-block w-14.25 h-6 text-[#252525] shrink-0 bg-#5CE5C1 text-2.5 font-500 lh-6 text-center rounded-10px"}>加速计划
+                </div>
+                <div className={"inline-block ml-10px mr-16px"}>
+                    <Image src={"/images/special-column/Share-black.png"} alt={"心智与阅读"} width={12} height={12}/>
+                </div>
+            </div>
+
+            {/*头像，昵称，时间*/}
+            <div>
+                心智与阅读
+            </div>
+            <div className="flex mt-10px items-center space-y-0 mb-22px">
+
+                {/*左边头像*/}
+                <div className={""}>
+                    <div>
+                        <Image src={"/images/special-column/Ellipse 2.png"} alt={"心智与阅读"} width={33} height={33}/>
+                    </div>
+                </div>
+                {/*昵称，日期，VIP*/}
+                <div>
+                    <div className={"flex items-center"}>
+                        <div className={"text-[#999] text-2.75 font-not-italic font-500 lh-18px ml-5px"}>芋圆</div>
+                        <div>
+                            <Image src={"/images/special-column/Group 225.png"} alt={"心智与阅读"} width={12}
+                                   height={12} className={"lh-0"} style={{marginLeft: "2.5px"}}/>
+                        </div>
+                    </div>
+                    <div className={"text-[#999] text-2.75 font-not-italic font-500 lh-18px ml-5px"}>3.02 12:30发布
+                    </div>
+                </div>
+            </div>
+
+            {/*内容*/}
+            <div>
+                <Image src={"/images/special-column/Rectangle 442.png"} alt={"心智与阅读"} width={343} height={161}/>
+            </div>
+            <div className={"w-91% mt-24px shrink-0 text-[#666] text-3.5 font-not-italic font-400 lh-[120%]"}>
+                情绪价值波动，上上签，愤怒，变化，偏执，创造 情绪价值波动，上上签，愤怒，变化，偏执，创造 情绪价值波动，上上签，愤怒，变化，偏执，创造
+            </div>
+        </div>
+
+        {/*答题*/}
+        <div className={"mx-16px pb-50px "}>
+            <div className={" shrink-0 text-[#252525] text-3.75 font-not-italic font-500 lh-6 mb-16px"}>
+                题目内容是这个的那我随便打一下，选择题，字数多了你就下去吧...
+            </div>
+            {/*选项*/}
+            <div className={"w-98% h-40px shrink-0 border-rd-1.25 border-1 border-solid border-[#EAF7EE] bg-[#DAF9F1] py-10px mt-8px"}>
+                <div className={"shrink-0 text-[#1DB48D] text-3.5 font-not-italic font-500 pl-21px"}>正确答案</div>
+            </div>
+            <div className={"w-98% h-40px shrink-0 border-rd-1.25 border-1 border-solid border-[#EAF7EE] bg-[#ffebec] py-10px mt-8px"}>
+                <div className={"shrink-0 text-[#f04444] text-3.5 font-not-italic font-500 pl-21px"}>错误答案</div>
+            </div>
+            <div className={"w-98% h-40px shrink-0 border-rd-1.25 border-1 border-solid border-[#EAF7EE] bg-[#ffffff] py-10px mt-8px"}>
+                <div className={"shrink-0 text-[#b5b5b5] text-3.5 font-not-italic font-500 pl-21px"}>选项在这</div>
+            </div>
+            <div className={"w-98% h-40px shrink-0 border-rd-1.25 border-1 border-solid border-[#EAF7EE] bg-[#ffffff] py-10px mt-8px "}>
+                <div className={"shrink-0 text-[#b5b5b5] text-3.5 font-not-italic font-500 pl-21px"}>选项在这</div>
+            </div>
+        </div>
+    </div>
+}
+
+export default Page;
