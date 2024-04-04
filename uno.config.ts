14f599140d11df27461fbe1cb6db3b823aa294aa
opacity <Wade2513473008@gmail.com>
2024-04-04 15:18:18 +0800

    fix(user)用户页更改背景渐变色表示和unoconfig三个渐变色声明

--- uno.config.ts
+++ uno.config.ts
@@ -7,9 +7,9 @@ export default defineConfig({
     theme: {
         colors: {
             // ...
-            'custom-blue': 'rgba(205, 233, 251, 1)',
-            'custom-teal': 'rgba(236, 249, 249, 1)',
-            'custom-light-blue': 'rgba(245, 245, 251,0.6)'
+            'custom-user_gradient_1': 'rgba(205, 233, 251, 1)',
+            'custom-user_gradient_2': 'rgba(236, 249, 249, 1)',
+            'custom-user_gradient_3': 'rgba(245, 245, 251,0.6)'
         }
     }
 })
