027c5bc7bcbd434671bd3da137f5cbc0afc17a18
opacity <2513473008@qq.com>
2024-04-19 22:37:55 +0800

    fix(user)修改页面链接

--- src/app/user/page.tsx
+++ src/app/user/page.tsx
@@ -1,6 +1,8 @@
 // page.tsx
 import Image from "next/image";
 import React from "react";
+import Navbar from "@/app/_components/common/Navbar";
+import Link from "next/link";
 
 function User() {
     return (
@@ -9,7 +11,7 @@ function User() {
             {/* 顶部 */}
             <div className="pl-4 pt-9">
                 <div className="flex flex-row items-center">
-                    <Image src="/images/user/avatar.svg" alt="avatar" width={83} height={83} className="my-4" />
+                    <Image src="/images/user/avatar.svg" alt="avatar" width={83} height={83} className={"w-20.75 h-20.75 fill-url( stroke-0.25 stroke-[#FFF]"} />
 
                     {/* 用户信息 */}
                     <div className="ml-3 flex-1">
@@ -35,21 +37,21 @@ function User() {
                         className="w-20 text-[#252525] text-2.5 font-500 lh-6 rounded-xl mr-4 flex flex-row bg-white pl-2.5 items-center ">
                         <Image src="/images/user/icon_edit.png" alt="icon" width={14} height={14}
                             className="w-4 h-4 mr-1" />
-                        <a href="#">编辑资料</a>
+                        <Link href="user-message">编辑资料</Link>
                     </div>
                 </div>
             </div>
 
 
             {/* 专栏、小课区域 */}
-            <div className="w-85.75 h-63.75 border-rd-2.5 bg-#FFF ml-4 pl-4 pt-4 mt-4">
+            <div className="w-85.75 h-63.75 border-rd-2.5 bg-#FFF ml-2 pl-4 pt-4 mt-4">
                 {/* 导航区域 */}
                 <div className="flex">
-                    <a href="#"
-                        className="w-7 h-6 mr-8 text-[#B5B5B5] font-source-han-sans-sc text-sm font-normal leading-6">专栏</a>
+                    <Link href="#"
+                        className="w-7 h-6 mr-8 text-[#B5B5B5] font-source-han-sans-sc text-sm font-normal leading-6">专栏</Link>
                     <div className={"flex-col"}>
-                        <a href="#"
-                           className="text-[#252525] text-3.5 font-500 lh-6">小课</a>
+                        <Link href="#"
+                           className="text-[#252525] text-3.5 font-500 lh-6">小课</Link>
                         <div className={"ml-2.25 mt-1 w-2.75 h-1 border-rd-2 bg-[#45E1B8]"}></div>
                     </div>
 
@@ -78,60 +80,60 @@ function User() {
             </div>
 
             {/*  我的服务模块*/}
-            <div className={"w-85.75 h-51.5 border-rd-2.5 bg-[#FFF] mx-auto mt-1.5  pt-4.5"}>
+            <div className={"w-85.75 h-51.5 border-rd-2.5 bg-[#FFF] ml-2 mt-1.5  pt-4.5 mb-3"}>
                 {/*标题*/}
                 <h2 className={"text-[#252525] text-3.5 font-500 lh-6 pl-5.5"}>我的服务</h2>
 
                 <div className="service">
                     <ul className={"flex flex-wrap"}>
                         <li className={"flex flex-col items-center w-1/4 mt-4.5 mb-2.5"}>
-                            <a href="#" className="flex flex-col items-center">
+                            <Link href="user-detailed" className="flex flex-col items-center">
                                 <Image src={'/images/user/HomePage.svg'} alt={"个人主页"} width={"48"} height={"48"}
                                     className={"w-6 h-6"} />
                                 <p className={"w-11.5 text-[#252525] text-2.75 font-400 lh-6"}>个人主页</p>
-                            </a>
+                            </Link>
                         </li>
                         <li className={"flex flex-col items-center w-1/4 mt-4.5 mb-2.5"}>
-                            <a href="#" className="flex flex-col items-center">
+                            <Link href="#" className="flex flex-col items-center">
                                 <Image src={'/images/user/KnowledgePlanet.svg'} alt={"知识星球"} width={"48"} height={"48"}
                                     className={"w-6 h-6"} />
                                 <p className={"w-11.5 text-[#252525] text-2.75 font-400 lh-6"}>知识星球</p>
-                            </a>
+                            </Link>
                         </li>
                         <li className={"flex flex-col items-center w-1/4 mt-4.5 mb-2.5"}>
-                            <a href="#" className="flex flex-col items-center">
+                            <Link href="#" className="flex flex-col items-center">
                                 <Image src={'/images/user/Usage.svg'} alt={"使用说明"} width={"48"} height={"48"}
                                     className={"w-6 h-6"} />
                                 <p className={"w-11.5 text-[#252525] text-2.75 font-400 lh-6"}>使用说明</p>
-                            </a>
+                            </Link>
                         </li>
                         <li className={"flex flex-col items-center w-1/4 mt-4.5 mb-2.5"}>
-                            <a href="#" className="flex flex-col items-center">
+                            <Link href="wallet" className="flex flex-col items-center">
                                 <Image src={'/images/user/Income.svg'} alt={"收入提现"} width={"48"} height={"48"}
                                     className={"w-6 h-6"} />
                                 <p className={"w-11.5 text-[#252525] text-2.75 font-400 lh-6"}>收入提现</p>
-                            </a>
+                            </Link>
                         </li>
                         <li className={"flex flex-col items-center w-1/4 mt-4.5 mb-2.5"}>
-                            <a href="#" className="flex flex-col items-center">
+                            <Link href="#" className="flex flex-col items-center">
                                 <Image src={'/images/user/Feedback.svg'} alt={"意见反馈"} width={"48"} height={"48"}
                                     className={"w-6 h-6"} />
                                 <p className={"w-11.5 text-[#252525] text-2.75 font-400 lh-6"}>意见反馈</p>
-                            </a>
+                            </Link>
                         </li>
                         <li className={"flex flex-col items-center w-1/4 mt-4.5 mb-2.5"}>
-                            <a href="#" className="flex flex-col items-center">
+                            <Link href="#" className="flex flex-col items-center">
                                 <Image src={'/images/user/Contact.svg'} alt={"联系客服"} width={"48"} height={"48"}
                                     className={"w-6 h-6"} />
                                 <p className={"w-11.5 text-[#252525] text-2.75 font-400 lh-6"}>联系客服</p>
-                            </a>
+                            </Link>
                         </li>
                         <li className={"flex flex-col items-center w-1/4 mt-4.5 mb-2.5"}>
-                            <a href="#" className="flex flex-col items-center">
+                            <Link href="login" className="flex flex-col items-center">
                                 <Image src={'/images/user/SignOut.svg'} alt={"退出登录"} width={"48"} height={"48"}
                                     className={"w-6 h-6"} />
                                 <p className={"w-11.5 text-[#252525] text-2.75 font-400 lh-6"}>退出登录</p>
-                            </a>
+                            </Link>
                         </li>
 
 
@@ -142,6 +144,8 @@ function User() {
                 </div>
 
             </div>
+
+            <Navbar/>
         </div>
 
     )
