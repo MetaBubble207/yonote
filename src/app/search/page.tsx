272a4db30f3b714c09fe08b5ab7a23f4ff8ec649
opacity <c02cla07@gmail.com>
2024-04-22 02:19:04 +0800

    fix(recommend): 增加推荐页面内的的组件跳转

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -9,7 +9,8 @@ const Page=()=>{
             <div className="flex justify-between items-center w-85.75">
                 <div className="border-rd-13 h-8.5 w-73.75 bg-[#f5f7fb] flex items-center">
                     <Image src={"/images/search/search.svg"} alt="search" width={18} height={18} className="inline ml-5.25 w-4.5 h-4.5" />
-                    <div className="text-3.25 text-[#252525] lh-8 ml-1.6 w-32.5 h-7.5 pl-1 border-rd-13">心智阅读</div>
+                    
+                    <input type="text" placeholder="心智阅读" className="text-3.25 text-[#252525] lh-8 ml-1.6 w-62.5 h-7.5 pl-1 bg-transparent outline-none "></input>
                 </div>
                 
                 <div className="text-3.75 text-[#252525] lh-6 w-8.25 h-5.5 ">取消</div>
