51ee5f4788a5be8086a6ba4a32dae51b537fbfa1
opacity <2807268479@qq.com>
2024-04-21 16:11:45 +0800

    feat(left-compass)完成了侧栏的基础样式

--- src/app/writer/compass/page.tsx
+++ src/app/writer/compass/page.tsx
@@ -2,10 +2,11 @@ import React from "react";
 import Image from "next/image";
 
 const Compass= () => {
+    const name = "我的昵称"
 
     return (
         <div>
-            <div className="w-360 h-17.5 shrink-0 bg-[#FFF]">
+            <div className="w-360 h-17.5 shrink-0 bg-[#FFF] flex items-center">
                 <div className="flex items-center w-107.55675 h-11.75 shrink-0 ml-7.1975 mt-2.875">
                     <div className="inline-flex w-20 h-9.48025 items-center">
                         <Image src={"/images/logo.svg"} alt={"logo"} width={30.3} height={30.42} className="w-7.57425 h-8.35625 shrink-0"></Image>
@@ -18,17 +19,29 @@ const Compass= () => {
                             </div>
                         </div>
                     </div>
-                    <div className="text-[#323232] text-3.5 font-not-italic font-400 lh-6 ml-17.5575 ">
+                    <div className="text-[#323232] text-3.5 font-400 lh-6 ml-17.5575 ">
                         官网
                         <button></button>
                     </div>
-                    <div className="text-[#323232] text-3.5 font-not-italic font-400 lh-6 ml-15">
+                    <div className="text-[#323232] text-3.5 font-400 lh-6 ml-15">
                         专栏申请
                         <button></button>
                     </div>
-                    <div className="text-[#323232] text-3.5 font-not-italic font-400 lh-6 ml-15">
+                    <div className="text-[#323232] text-3.5 font-400 lh-6 ml-15">
                         反馈社区
                     </div>
+                </div>
+                <div>
+                    <Image src={"/images/writer/Ellipse 7.svg"} alt="Ellipse 7" width={44} height={44} className="w-11 h-11 shrink-0 border-rd-11 ml-183"></Image>
+                </div>
+                <div className="ml-3.4275">
+                    {name}
+                </div> 
+                <div>
+                    <select name="" id="">
+                        <option value=""> </option>
+                        <option value="">退出登录</option>
+                    </select>
                 </div>
             </div>
         </div>
