e5b1c38c328104dee6b892459c0b9f8747b676e2
opacity <2513473008@qq.com>
2024-04-28 18:19:24 +0800

    fix(slider):修改输入进度条组件

---
+++ src/app/_components/slider/page.tsx
@@ -0,0 +1,56 @@
+'use client'
+
+import React, { useState } from 'react';
+import { ConfigProvider, InputNumberProps } from 'antd';
+import { Col, InputNumber, Row, Slider, Space } from 'antd';
+
+const IntegerStep: React.FC = () => {
+    const [inputValue, setInputValue] = useState(1);
+
+    const onChange: InputNumberProps['onChange'] = (newValue) => {
+        setInputValue(newValue as number);
+    };
+
+    return (
+        <div>
+            <Row>
+                <Col span={24}>
+                    <Slider className={'mr-18.7025'}
+                        min={0}
+                        max={100}
+                        onChange={onChange}
+                        value={typeof inputValue === 'number' ? inputValue : 0}
+                    />
+                </Col>
+            </Row>
+            <Row className='flex justify-between mr-18.7025'>
+                <Col>
+                    <span>0%</span>
+                </Col>
+                <Col>
+                    <span>{inputValue}%</span>
+                </Col>
+            </Row>
+        </div>
+    );
+};
+
+const MySlider = () => (
+    <ConfigProvider
+        theme={{
+            components: {
+                Slider: {
+                    handleActiveColor: '#1DB48D',
+                    trackBg: '#1DB48D',
+                    trackHoverBg: '#1DB48D',
+                    handleColor: '#1DB48D',
+                    colorPrimaryBorderHover: '#1DB48D',
+                },
+            },
+        }}
+    >
+        <IntegerStep />
+    </ConfigProvider>
+);
+
+export default MySlider;
