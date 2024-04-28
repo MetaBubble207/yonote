d7fbcb2686751c34d67e04f481e124c6a65e5d9c
opacity <2165768894@qq.com>
2024-04-29 00:41:19 +0800

    fix(homepage):添加数据，完善图表

--- src/app/_components/homepage/Daterange.tsx
+++ src/app/_components/homepage/Daterange.tsx
@@ -1,6 +1,36 @@
-const Daterange = () => {
+import React, { useState } from 'react';
+import { ConfigProvider, DatePicker, Space } from 'antd';
+import locale from 'antd/locale/zh_CN';
+import dayjs, { Dayjs } from 'dayjs';
+import 'dayjs/locale/zh-cn';
+import data from './imitate.json'
+import Echarts from './Echarts';
+import { log } from 'console';
+
+const Daterange: React.FC = () => {
+
+    const [selectedDates, setSelectedDates] = useState<[Date, Date] | null>(null);
+    let dateStrings: string[] = [];
+    const onRangeChange = (dates: null | (Dayjs | null)[], dateStrings: string[]) => {
+        if (dates) {
+            console.log(dateStrings[0], dateStrings[1]);
+        } else {
+            console.log('Clear');
+        }
+    };
+    console.log(1232, dateStrings);
+
+
+    const disabledDate = (time: any) => {
+        const today = new Date();
+        today.setHours(0, 0, 0, 0);
+        return time && time > today;
+    };
+    dayjs.locale('zh-cn');
     return (
-        <div>123</div>
-    )
+        <ConfigProvider locale={locale}>
+            <DatePicker.RangePicker style={{ width: '100%' }} disabledDate={disabledDate} onChange={onRangeChange} />
+        </ConfigProvider>
+    );
 }
 export default Daterange;
