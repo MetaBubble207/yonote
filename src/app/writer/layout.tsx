1f728e7066f4fcb2078885a88cb0bfab90d65b2d
opacity <2513473008@qq.com>
2024-04-29 16:05:49 +0800

    fix(writer-layout):修改Compass 和 LeftCompass的引入

--- src/app/writer/layout.tsx
+++ src/app/writer/layout.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import './writer.css';
-import Compass from "../_components/writer/compass";
-import LeftCompass from "../_components/writer/left_compass";
+import Compass  from "@/app/_components/writer/compass";
+import LeftCompass from "@/app/_components/writer/left_compass";
 
 const dialogLayout=({children}:{children:React.ReactNode})=>{
     return(
@@ -9,9 +9,9 @@ const dialogLayout=({children}:{children:React.ReactNode})=>{
             <body>
                     <div className="w-100% min-h-screen bg-[#F6F6F6]" >
                         <Compass></Compass>
-                        <div className="flex w-full min-h-screen">
+                        <div className="flex w-100%">
                             <LeftCompass></LeftCompass>
-                            <div className="w-full h-80vh ml-64.77925 mt-17.5 mb-10">
+                            <div className="w-100%  ml-64.77925 mt-17.5 mb-10">
                                 {children}
                             </div>
                         </div>
