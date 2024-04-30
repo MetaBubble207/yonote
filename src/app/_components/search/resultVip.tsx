5458f90145f7eed3118a63817632d39365e15ba4
opacity <c02cla07@gmail.com>
2024-05-01 00:03:23 +0800

    fix(search,find): 封装find和search页面的组件

---
+++ src/app/_components/search/resultVip.tsx
@@ -0,0 +1,21 @@
+import Image from "next/image"
+
+const ResultVip =()=>{
+    return (
+        <div className="h-23 w-full mt-6 bg-[#FFF] flex items-start">  
+            <div className="flex items-start">
+                <Image src={"/images/search/cover.svg"} alt="cover" width={18.2} height={24.8} className="w-17.25 h-23" />
+                <div className="pl-3 flex flex-col">
+                    <div className="text-[#252525] text-3.75 font-500 lh-6">「心智与阅读系列：和人家啊已经 是豪丝娜的」</div>
+                    <div className="flex items-center">
+                        <Image src={"/images/search/icon1.svg"} alt="icon1" width={14} height={14} className="w-3.5 h-3.5" />
+                        <div className="text-[#999]  text-2.75  font-500 lh-6 ml-1 mr1.25">芋圆</div>
+                        <Image src={"/images/search/vip.svg"} alt="vip" width={12} height={12} className="w-3 h-3" />
+                        </div>
+                    <div className=" text-[#B5B5B5]  text-2.75 font-350 lh-6 ">03.03发布</div>
+                    </div>
+                </div>
+            </div>
+    )
+}
+export default ResultVip;
