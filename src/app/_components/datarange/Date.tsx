fee3964b351b5a9ba26be322389d378201f1d78c
opacity <2513473008@qq.com>
2024-04-30 11:37:21 +0800

    fix(Date.tsx):修改日期组件，限定为不能选择当前时段之后的日期

--- src/app/_components/datarange/Date.tsx
+++ src/app/_components/datarange/Date.tsx
@@ -1,6 +1,6 @@
 'use client'
 import {DatePicker} from 'antd';
-import dayjs from 'dayjs';
+import dayjs, {Dayjs} from 'dayjs';
 import customParseFormat from 'dayjs/plugin/customParseFormat';
 import weekday from "dayjs/plugin/weekday"
 import localeData from "dayjs/plugin/localeData"
@@ -12,13 +12,29 @@ dayjs.extend(localeData)
 dayjs.extend(customParseFormat);
 const {RangePicker} = DatePicker;
 
+interface DisabledDateProps {
+    currentDate: Dayjs;
+    info?: { from?: Dayjs };
+}
+
+interface DisabledDateProps {
+    currentDate: Dayjs;
+    info?: { from?: Dayjs };
+}
+
+const disabledDate = (props: DisabledDateProps) => {
+    const { currentDate, info } = props;
+    // 禁用当前日期之后的日期
+    return currentDate.isAfter(dayjs(), 'day');
+};
+
 const dateFormat = 'YYYY/MM/DD';
 const Date = () => {
     return (
         <div className='w-56 h-8'>
             {/* eslint-disable-next-line @typescript-eslint/no-unsafe-call */}
             <RangePicker defaultValue={[dayjs('2015/01/01', dateFormat), dayjs('2015/01/01', dateFormat)]}
-                         format={dateFormat}/>
+                         format={dateFormat} disabledDate={(currentDate, info) => disabledDate({ currentDate, info })}/>
         </div>
     )
 }
