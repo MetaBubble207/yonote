d8b077f31455ae77ab6839db4e62ef2543337e1c
opacity <2513473008@qq.com>
2024-04-30 13:29:23 +0800

    fix(Date.tsx):修改日期组件

--- src/app/_components/datarange/Date.tsx
+++ src/app/_components/datarange/Date.tsx
@@ -1,10 +1,14 @@
 'use client'
-import {DatePicker} from 'antd';
+import {ConfigProvider, DatePicker} from 'antd';
 import dayjs, {Dayjs} from 'dayjs';
 import customParseFormat from 'dayjs/plugin/customParseFormat';
 import weekday from "dayjs/plugin/weekday"
 import localeData from "dayjs/plugin/localeData"
 import React from "react";
+import 'dayjs/locale/zh-cn';
+import locale from "antd/locale/zh_CN";
+
+dayjs.locale('zh-cn');
 dayjs.extend(weekday)
 // eslint-disable-next-line @typescript-eslint/no-unsafe-call
 dayjs.extend(localeData)
@@ -24,15 +28,17 @@ const disabledDate = (props: DisabledDateProps) => {
     // 禁用当前日期之后的日期
     return currentDate.isAfter(dayjs(), 'day');
 };
-
+dayjs.locale('zh-cn');
 const dateFormat = 'YYYY/MM/DD';
 const Date = () => {
     return (
         <div className='w-56 h-8'>
+            <ConfigProvider locale={locale}>
+                <RangePicker  defaultValue={[dayjs('2015/01/01', dateFormat), dayjs('2015/01/01', dateFormat)]}
+                              format={dateFormat} disabledDate={(currentDate, info) => disabledDate({ currentDate, info })}/>
+            </ConfigProvider>
             {/* eslint-disable-next-line @typescript-eslint/no-unsafe-call */}
-            <RangePicker defaultValue={[dayjs('2015/01/01', dateFormat), dayjs('2015/01/01', dateFormat)]}
-                         format={dateFormat} disabledDate={(currentDate, info) => disabledDate({ currentDate, info })}/>
-        </div>
+           </div>
     )
 }
 export default Date
