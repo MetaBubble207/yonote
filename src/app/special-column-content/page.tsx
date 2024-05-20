01ec394cabaeeabc004dc4fcb114b6bf40b51502
opacity <2165768894@qq.com>
2024-05-20 14:20:18 +0800

    feat(post):添加文章专栏上一篇事件

--- src/app/special-column-content/page.tsx
+++ src/app/special-column-content/page.tsx
@@ -3,9 +3,9 @@ import Image from "next/image";
 import Link from "next/link";
 import { api } from "@/trpc/react";
 import { useEffect, useState } from "react";
-import {useRouter, useSearchParams} from "next/navigation";
+import { useRouter, useSearchParams } from "next/navigation";
 import useLocalStorage from "@/tools/useStore";
-import {timeToDateString} from "@/tools/timeToString";
+import { timeToDateString } from "@/tools/timeToString";
 
 const Page = () => {
   // function formatTime(date: Date) {
@@ -44,12 +44,12 @@ const Page = () => {
     setIsHeartFilled(!isHeartFilled);
     if (isHeartFilled) {
       setLikecount(likecount - 1);
-    }else{
+    } else {
       setLikecount(likecount + 1);
     }
   };
 
-  const [token] =   useLocalStorage("token", null);
+  const [token] = useLocalStorage("token", null);
 
   const postData = api.post.getById.useQuery({
     id: columnId,
@@ -62,10 +62,10 @@ const Page = () => {
   const [likecount, setLikecount] = useState(0);
 
   const update = api.post.create.useMutation({
-    onSuccess: (r)=>{
-        console.log('123' + r)
+    onSuccess: (r) => {
+      console.log('123' + r)
     },
-    onError: (e)=>{
+    onError: (e) => {
 
     }
   })
@@ -74,35 +74,36 @@ const Page = () => {
   //   id: 16,
   // })
 
+
   useEffect(() => {
-   if(postData){
-     if (postData.name) {
-       const pubTime = postData.createdAt;
-       setName(postData.name);
-       setDate(timeToDateString(pubTime));
-     } else {
-       setDate("");
-       setName("");
-     }
+    if (postData) {
+      if (postData.name) {
+        const pubTime = postData.createdAt;
+        setName(postData.name);
+        setDate(timeToDateString(pubTime));
+      } else {
+        setDate("");
+        setName("");
+      }
 
-     if (postData.tag) {
-       setTags(postData.tag.split(","));
-     } else {
-       setTags([]);
-     }
+      if (postData.tag) {
+        setTags(postData.tag.split(","));
+      } else {
+        setTags([]);
+      }
 
-     if (postData.content) {
-       setContent(postData.content);
-     } else {
-       setContent("");
-     }
+      if (postData.content) {
+        setContent(postData.content);
+      } else {
+        setContent("");
+      }
 
-     if (postData.likeCount) {
-       setLikecount(postData.likeCount);
-     } else {
-       setLikecount(0);
-     }
-   }
+      if (postData.likeCount) {
+        setLikecount(postData.likeCount);
+      } else {
+        setLikecount(0);
+      }
+    }
   }, [postData]);
 
   return (
@@ -208,7 +209,7 @@ const Page = () => {
           })}
           <div className="flex" onClick={handleClick}>
             <Image
-              src={isHeartFilled?"/images/special-column/heart red.png":"/images/special-column/heart 1.png"}
+              src={isHeartFilled ? "/images/special-column/heart red.png" : "/images/special-column/heart 1.png"}
               alt={"爱心"}
               width={18}
               height={18}
@@ -259,40 +260,70 @@ const Page = () => {
 
             {/*上一篇下一篇*/}
             <div className="flex mt-8px">
-              <div className="flex flex-col">
-                <div className={"flex items-center"}>
-                  <div>
+              {/* 上一篇 */}
+              {chapter === 1 ? (
+                <div className={"flex flex-col"}>
+                  <div className={"flex items-center"}>
                     <Image
                       src={"/images/special-column/Double-left (双左).png"}
                       alt={"心智与阅读"}
                       width={14}
                       height={14}
                     />
+                    <div
+                      className={
+                        "text-[#333] text-3 font-not-italic font-400 lh-6 ml-5px"
+                      }
+                    >
+                      上一篇
+                    </div>
                   </div>
                   <div
                     className={
-                      "text-[#333] text-3 font-not-italic font-400 lh-6 ml-5px"
+                      "w-27.6665 text-[#333]  text-3 font-not-italic font-400 lh-6"
                     }
                   >
-                    上一篇
+                    已经是第一篇了
                   </div>
                 </div>
-                <div
-                  className={
-                    "w-27.6665 text-[#333]  text-3 font-not-italic font-400 lh-6"
-                  }
-                >
-                  回忆·后来的我们毕业了
-                </div>
-              </div>
-              <div className="flex flex-col ml-auto ">
+              )
+                : (
+                  <Link className="flex flex-col" href={`../special-column-content?c=${chapter - 1}&id=${columnId}`}>
+                    <div className={"flex items-center"}>
+                      <div>
+                        <Image
+                          src={"/images/special-column/Double-left (双左).png"}
+                          alt={"心智与阅读"}
+                          width={14}
+                          height={14}
+                        />
+                      </div>
+                      <div
+                        className={
+                          "text-[#333] text-3 font-not-italic font-400 lh-6 ml-5px"
+                        }
+                      >
+                        上一篇
+                      </div>
+                    </div>
+                    <div
+                      className={
+                        "w-27.6665 text-[#333]  text-3 font-not-italic font-400 lh-6"
+                      }
+                    >
+                      回忆·后来的我们毕业了
+                    </div>
+                  </Link>
+                )}
+
+              <Link className="flex flex-col ml-auto " href={`../special-column-content?c=${chapter + 1}&id=${columnId}`}>
                 <div className={"flex items-center justify-end"}>
                   <div
                     className={
                       "text-[#333] text-3 font-not-italic font-400 lh-6 "
                     }
                   >
-                    上一篇
+                    下一篇
                   </div>
                   <div>
                     <Image
@@ -310,7 +341,7 @@ const Page = () => {
                 >
                   回忆·后来的我们毕业了
                 </div>
-              </div>
+              </Link>
             </div>
           </div>
         </div>
