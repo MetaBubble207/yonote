748b43c2ce5a7808822f49975a773605c1ae7711
opacity <c02cla07@gmail.com>
2024-05-27 21:38:06 +0800

    feat（wallet）：实现账户余额、冻结金额、提现金额查询

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -5,6 +5,7 @@ import { testRouter } from "@/server/api/routers/test";
 import { columnRouter } from "@/server/api/routers/column";
 import { draftRouter } from "./routers/draft";
 import { postLikeRouter } from "./routers/postLike";
+import { walletRouter } from "./routers/wallet";
 
 /**
  * This is the primary router for your server.
@@ -18,6 +19,7 @@ export const appRouter = createTRPCRouter({
   column: columnRouter,
   draft: draftRouter,
   like: postLikeRouter,
+  wallet:walletRouter,
 });
 
 // export type definition of API
