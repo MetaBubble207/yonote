e8b13e6c01f1d58289b044f7393fb11d03d201f9
opacity <c02cla07@gmail.com>
2024-05-28 16:00:27 +0800

    fix(find): 活动截止不显示在页面

--- src/app/_components/find/activities.tsx
+++ src/app/_components/find/activities.tsx
@@ -2,6 +2,7 @@
 import Image from "next/image";
 import React, { useState,useEffect  } from "react";
 import { api } from "@/trpc/react";
+import { getCurrentTime } from "@/tools/getCurrentTime";
 
 const Activities =()=>{
     const [data, setData] = useState(null);
@@ -11,15 +12,25 @@ const Activities =()=>{
 
     // 在数据加载完成时更新状态
     useEffect(() => {
-        if (queryData) {
-            //排列数据，让进行中的活动排列靠前
-            const sortedData = [...queryData].sort((a, b) => {
-                if (a.isEnd === b.isEnd) return 0;
-                return a.isEnd ? 1 : -1; // 进行中的活动排在前面
+    //     if (queryData) {
+    //         //排列数据，让进行中的活动排列靠前
+    //         const sortedData = [...queryData].sort((a, b) => {
+    //             if (a.isEnd === b.isEnd) return 0;
+    //             return a.isEnd ? 1 : -1; // 进行中的活动排在前面
+    //         });
+    //         setData(sortedData);
+    //     }
+        if (queryData){
+            const currentTime = getCurrentTime();
+            //过滤为截止的活动
+            const goingData = queryData.filter(item => {
+                const endDate = new Date(item.enddate);
+                return endDate > currentTime;
             });
-            setData(sortedData);
+            setData(goingData);
         }
     }, [queryData]);
+
 
     return(
         <div>
@@ -27,17 +38,29 @@ const Activities =()=>{
                 <div className="inline mt-4 h-36.25 w-full border-rd-4 bg-[#FFF] flex relative " key={item.id}>
                     <div className="inline mt-2.5 ml-2.5 h-31.25 w-80.75 item-center flex  ">
                         <div className=" flex flex-col">
-                            <div className=" w-16 text-[#252525] text-3.75  font-500 lh-6 ml-46 mt-1.75" >{item.name}</div>
+                            <div className="text-[#252525] text-3.75  font-500 lh-6 ml-46 mt-1.75" >{item.name}</div>
                             <div className=" mt-1.25 ml-46 w-13.75 text-[#666] text-3.25  font-400 lh-[120%]">{item.introduction}</div> 
                         </div>
                         <Image src={"/images/subscribe/acti-cover.svg"} alt="acti-cover" width={16} height={12} className="border-rd=4 w-41 h-31.25 absolute top-2.5 left-2.5 "/> 
                         
-                        <div className={` absolute top-2.5 left-2.5 w-11.75 h-5.25 border-rd-[0px_25px_25px_0px] bg-[#B8BBCC]}`}>
+                        <div className={` absolute top-2.5 left-2.5 w-11.75 h-5.25 border-rd-[0px_25px_25px_0px] bg-[#4EDFE9]`}>
                             <div className="ml-1.75 mt-1 text-[#FFF] text-2.5 font-500 lh-[120%]">进行中</div>
                         </div>
-                        <button className={`ml-61 mt-23.5 w-18.25 h-6.25 bg-[rgba(218,249,241,0.35)] border-rd-5.25 text-[rgba(29,180,141,0.35)] font-500 lh-6 text-center text-3 absolute right-4 bottom-4`}>
+                        <button className={`ml-61 mt-23.5 w-18.25 h-6.25 bg-[#DAF9F1] border-rd-5.25 text-[#1DB48D] font-500 lh-6 text-center text-3 absolute right-4 bottom-4`}>
                             立即查看
                         </button>  
+
+                             
+                    </div>
+                </div>
+            ))} 
+        </div> 
+    )
+}
+export default Activities;
+
+
+
 
                         {/* 根据 item.isEnd 的值选择不同的样式 */}
                         {/* <div className={` absolute top-2.5 left-2.5 w-11.75 h-5.25 border-rd-[0px_25px_25px_0px] ${item.isEnd ? 'bg-[#B8BBCC]' : 'bg-[#4EDFE9]'}`}>
@@ -48,12 +71,3 @@ const Activities =()=>{
                         {/* <button className={`ml-61 mt-23.5 w-18.25 h-6.25 ${item.isEnd ? 'bg-[rgba(218,249,241,0.35)] border-rd-5.25 text-[rgba(29,180,141,0.35)]' : 'bg-[#DAF9F1] border-rd-5.25 text-[#1DB48D]'} font-500 lh-6 text-center text-3 absolute right-4 bottom-4`}>
                             立即查看
                         </button>     */}
-                        
-                             
-                    </div>
-                </div>
-            ))} 
-        </div> 
-    )
-}
-export default Activities;
