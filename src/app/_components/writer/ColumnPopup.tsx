2810d37ca59fdcb1e9f00e1cbcc32b97b0b4140f
opacity <c02cla07@gmail.com>
2024-05-29 21:25:56 +0800

    feat(homepage)：弹窗查询专栏名称，点击后回传数据，显示选中的专栏数据

--- src/app/_components/writer/ColumnPopup.tsx
+++ src/app/_components/writer/ColumnPopup.tsx
@@ -1,5 +1,75 @@
-// import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import Image from 'next/image';
+import { api } from "@/trpc/react";
+
+interface CarouselProps {
+  images: string[];
+  onImageClick: (index: number, column: { id: string; name: string; }) => void;
+}
+
+const ColumnPopup: React.FC<CarouselProps> = ({ images, onImageClick }) => {
+  const [currentIndices, setCurrentIndices] = useState([0, 1, 2, 3]);
+  const [columnData, setColumnData] = useState<{ id: string; name: string; }[]>([]);
+  const { data: queryData } = api.column.getAll.useQuery();
+
+  useEffect(() => {
+    if (queryData) {
+      setColumnData(queryData.map((column: { id: string; name: string; }) => ({ id: column.id, name: column.name })));
+    }
+  }, [queryData]);
+
+  const goToNextImage = () => {
+    setCurrentIndices((prevIndices) => {
+      const nextIndices = prevIndices.map((index) => (index + 4) % images.length);
+      return nextIndices;
+    });
+  };
+
+  const goToPrevImage = () => {
+    setCurrentIndices((prevIndices) => {
+      const updatedIndices = prevIndices.map((index) => (index - 4 + images.length) % images.length);
+      return updatedIndices;
+    });
+  };
+
+  const handleImageClick = (index: number) => {
+    onImageClick(index, columnData[index]);
+  };
+
+  return (
+    <div className='w-213.34 h-94.7 bg-white border-0.5 border-solid border-#D9D9D9 border-rd-2 flex items-center'>
+      <button onClick={goToPrevImage} className='ml-6.6'>
+        <Image src={'/images/writer/edit/left-c.svg'} alt={'left-c'} width={24} height={24} />
+      </button>
+      <div className='flex justify-between w-full px-8.2'>
+        {currentIndices.map((index) => (
+          <button key={index} onClick={() => handleImageClick(index)} className='flex flex-col items-center w-1/4'>
+            <img
+              src={images[index]}
+              alt={`图片${index + 1}`}
+              width={160}
+              height={51.5}
+              className='block'
+            />
+            <span className='mt-2'>
+              {columnData[index]?.name || 'Loading...'}
+            </span>
+          </button>
+        ))}
+      </div>
+      <button onClick={goToNextImage} className='mr-6.6'>
+        <Image src={'/images/writer/edit/right-c.svg'} alt={'right-c'} width={24} height={24} />
+      </button>
+    </div>
+  );
+};
+
+export default ColumnPopup;
+
+// import React, {useEffect, useState } from 'react';
 // import Image from 'next/image';
+// import { api } from "@/trpc/react";
+
 
 // interface CarouselProps {
 //   images: string[];
@@ -8,6 +78,15 @@
 
 // const ColumnPopup: React.FC<CarouselProps> = ({ images, onImageClick }) => {
 //   const [currentIndices, setCurrentIndices] = useState([0, 1, 2, 3]);
+//   const [columnName, setColumnName] =useState<string[]>([]);
+//   const {data: queryData} = api.column.getAll.useQuery();
+
+//   useEffect(() => {
+//     if (queryData) {
+//       const names = queryData.map((column: { name: string }) => column.name);
+//       setColumnName(names);
+//     }
+//   }, [queryData]);
 
 //   const goToNextImage = () => {
 //     setCurrentIndices((prevIndices) => {
@@ -27,91 +106,36 @@
 //     onImageClick(index);
 //   };
 
-// //   return (
-// //     <div className='w-213.34 h-94.7 bg-white border-0.5 border-solid border-#D9D9D9 border-rd-2 flex items-center'>
-// //       <button onClick={goToPrevImage} className='ml-6.6'>
-// //         <Image src={'/images/writer/edit/left-c.svg'} alt={'left-c'} width={24} height={24}></Image>
-// //       </button>
-// //       <div className='flex w-172 h-51.5 overflow-hidden ml-8.2 mr-8.2 '>
-// //         {images.map((image, index) => (
-          
-// //             <button  key={index} onClick={() => handleImageClick(index)}>
-// //               <img
-// //                 src={image}
-// //                 alt={`图片${index + 1}`}
-// //                 width={160}
-// //                 height={51.5}
-// //                 className={currentIndices.includes(index) ? 'block mr-4 ' : 'hidden mr-4 '}
-// //               />
-// //               <span className='mt-2'>专栏名称</span>
-// //             </button>
+//   return (
+//     <div className='w-213.34 h-94.7 bg-white border-0.5 border-solid border-#D9D9D9 border-rd-2 flex items-center'>
+//       <button onClick={goToPrevImage} className='ml-6.6'>
+//         <Image src={'/images/writer/edit/left-c.svg'} alt={'left-c'} width={24} height={24} />
+//       </button>
+//       <div className='flex justify-between w-full px-8.2'>
+//         {currentIndices.map((index) => (
+//           <button key={index} onClick={() => handleImageClick(index)} className='flex flex-col items-center w-1/4'>
+//             <img
+//               src={images[index]}
+//               alt={`图片${index + 1}`}
+//               width={160}
+//               height={51.5}
+//               className='block'
+//             />
+//             <span className='mt-2'>
+//               {columnName[index]}
+//             </span>
+            
+//           </button>
+//         ))}
+//       </div>
+//       <button onClick={goToNextImage} className='mr-6.6'>
+//         <Image src={'/images/writer/edit/right-c.svg'} alt={'right-c'} width={24} height={24} />
+//       </button>
+//     </div>
+//   );
+// };
 
-// //         ))}
-// //       </div>
-
-
-// //       <button onClick={goToNextImage}>
-// //         <Image src={'/images/writer/edit/right-c.svg'} alt={'right-c'} width={24} height={24}></Image>
-// //       </button>
-// //     </div>
-// //   );
-// // };
 // export default ColumnPopup;
 
-//在图片下面增加“专栏名称”
-import React, { useState } from 'react';
-import Image from 'next/image';
 
-interface CarouselProps {
-  images: string[];
-  onImageClick: (index: number) => void;
-}
 
-const ColumnPopup: React.FC<CarouselProps> = ({ images, onImageClick }) => {
-  const [currentIndices, setCurrentIndices] = useState([0, 1, 2, 3]);
-
-  const goToNextImage = () => {
-    setCurrentIndices((prevIndices) => {
-      const nextIndices = prevIndices.map((index) => (index + 4) % images.length);
-      return nextIndices;
-    });
-  };
-
-  const goToPrevImage = () => {
-    setCurrentIndices((prevIndices) => {
-      const updatedIndices = prevIndices.map((index) => (index - 4 + images.length) % images.length);
-      return updatedIndices;
-    });
-  };
-
-  const handleImageClick = (index: number) => {
-    onImageClick(index);
-  };
-
-  return (
-    <div className='w-213.34 h-94.7 bg-white border-0.5 border-solid border-#D9D9D9 border-rd-2 flex items-center'>
-      <button onClick={goToPrevImage} className='ml-6.6'>
-        <Image src={'/images/writer/edit/left-c.svg'} alt={'left-c'} width={24} height={24} />
-      </button>
-      <div className='flex justify-between w-full px-8.2'>
-        {currentIndices.map((index) => (
-          <button key={index} onClick={() => handleImageClick(index)} className='flex flex-col items-center w-1/4'>
-            <img
-              src={images[index]}
-              alt={`图片${index + 1}`}
-              width={160}
-              height={51.5}
-              className='block'
-            />
-            <span className='mt-2'>专栏名称</span>
-          </button>
-        ))}
-      </div>
-      <button onClick={goToNextImage} className='mr-6.6'>
-        <Image src={'/images/writer/edit/right-c.svg'} alt={'right-c'} width={24} height={24} />
-      </button>
-    </div>
-  );
-};
-
-export default ColumnPopup;
--- src/app/_components/writer/column.tsx
+++ src/app/_components/writer/column.tsx
@@ -1,66 +1,126 @@
-import React from "react";
+import React, { useState } from "react";
 import Image from "next/image";
 import ColumnPopup from "./ColumnPopup";
-import { useState } from "react";
-import { useEffect } from "react";
 
 const Column = () => {
-    const name = "专栏名称";
-    const images = [
-        '/images/writer/edit/Rectangle 2497.svg',
-        '/images/writer/edit/Rectangle 2497.svg',
-        '/images/writer/edit/Rectangle 2497.svg',
-        '/images/writer/edit/Rectangle 2497.svg',
-        '/images/writer/edit/Mask group.svg',
-        '/images/writer/edit/Mask group.svg',
-        '/images/writer/edit/Mask group.svg',
-        '/images/writer/edit/Mask group.svg',
-        '/images/writer/edit/Rectangle 2497.svg',
-        '/images/writer/edit/Rectangle 2497.svg',
-        '/images/writer/edit/Rectangle 2497.svg',
-        '/images/writer/edit/Rectangle 2497.svg',
-        '/images/writer/edit/Mask group.svg',
-        '/images/writer/edit/Mask group.svg',
-        '/images/writer/edit/Mask group.svg',
-        '/images/writer/edit/Mask group.svg',
-      ];
-    
-      // 当前图像的路径
-    const [currentImage, setCurrentImage] = useState(images[0]);
-    
-    const handleImageClick = (index: number) => {
-        console.log(`Clicked on image at index: ${index}`);
-        const handleChangeImage = () => {
-            // 根据需求设置新的图像路径
-            setCurrentImage(images[index]);
-        };
-        handleChangeImage();
-        setShowColumnPopup(!showColumnPopup);
-    };
+  const images = [
+    '/images/writer/edit/Rectangle 2497.svg',
+    '/images/writer/edit/Rectangle 2497.svg',
+    '/images/writer/edit/Rectangle 2497.svg',
+    '/images/writer/edit/Rectangle 2497.svg',
+    '/images/writer/edit/Mask group.svg',
+    '/images/writer/edit/Mask group.svg',
+    '/images/writer/edit/Mask group.svg',
+    '/images/writer/edit/Mask group.svg',
+    '/images/writer/edit/Rectangle 2497.svg',
+    '/images/writer/edit/Rectangle 2497.svg',
+    '/images/writer/edit/Rectangle 2497.svg',
+    '/images/writer/edit/Rectangle 2497.svg',
+    '/images/writer/edit/Mask group.svg',
+    '/images/writer/edit/Mask group.svg',
+    '/images/writer/edit/Mask group.svg',
+    '/images/writer/edit/Mask group.svg',
+  ];
 
-    const [showColumnPopup, setShowColumnPopup] = useState(false);
+  const [currentImage, setCurrentImage] = useState(images[0]);
+  const [columnName, setColumnName] = useState("专栏名称");
+  const [showColumnPopup, setShowColumnPopup] = useState(false);
+
+  const handleImageClick = (index: number, column: { id: string; name: string; }) => {
+    console.log(`Clicked on column: ${column.name}`);
+    // Set the column name and corresponding image
+    setCurrentImage(images[index]);
+    setColumnName(column.name);
+    setShowColumnPopup(false);
+  };
+
+  return (
+    <div>
+      <div className="w-14.5 h-18.75 shrink-0 border-rd-1.25 bg-#fff flex" onClick={() => setShowColumnPopup(!showColumnPopup)}>
+        <Image src={currentImage} alt="" width={58} height={75} className="w-14.5 h-18.75 shrink-0 border-rd-1.25"></Image>
+        <div className="mt-3">
+          <button>
+            <Image src={"/images/writer/edit/Switch.svg"} alt={""} width={14.09} height={14} className={"w-3.52225 h-3.5 shrink-0 ml-2"}></Image>
+          </button>
+          <div className="w-14.08925 text-[#323232] text-3.5 font-not-italic font-400 lh-6 ml-2 mt-2">
+            {columnName}
+          </div>
+        </div>
+      </div>
+      <div className="left-308px top-59px absolute">
+        {showColumnPopup && <ColumnPopup images={images} onImageClick={handleImageClick} />}
+      </div>
+    </div>
+  );
+}
+
+export default Column;
+
+// import React from "react";
+// import Image from "next/image";
+// import ColumnPopup from "./ColumnPopup";
+// import { useState } from "react";
+// import { useEffect } from "react";
+
+// const Column = () => {
+//     const name = "专栏名称";
+//     const images = [
+//         '/images/writer/edit/Rectangle 2497.svg',
+//         '/images/writer/edit/Rectangle 2497.svg',
+//         '/images/writer/edit/Rectangle 2497.svg',
+//         '/images/writer/edit/Rectangle 2497.svg',
+//         '/images/writer/edit/Mask group.svg',
+//         '/images/writer/edit/Mask group.svg',
+//         '/images/writer/edit/Mask group.svg',
+//         '/images/writer/edit/Mask group.svg',
+//         '/images/writer/edit/Rectangle 2497.svg',
+//         '/images/writer/edit/Rectangle 2497.svg',
+//         '/images/writer/edit/Rectangle 2497.svg',
+//         '/images/writer/edit/Rectangle 2497.svg',
+//         '/images/writer/edit/Mask group.svg',
+//         '/images/writer/edit/Mask group.svg',
+//         '/images/writer/edit/Mask group.svg',
+//         '/images/writer/edit/Mask group.svg',
+//       ];
+    
+//       // 当前图像的路径
+//     const [currentImage, setCurrentImage] = useState(images[0]);
+    
+//     const handleImageClick = (index: number) => {
+//         console.log(`Clicked on image at index: ${index}`);
+//         const handleChangeImage = () => {
+//             // 根据需求设置新的图像路径
+//             setCurrentImage(images[index]);
+//         };
+//         handleChangeImage();
+//         setShowColumnPopup(!showColumnPopup);
+//     };
+
+//     const [showColumnPopup, setShowColumnPopup] = useState(false);
    
 
 
     
-    return (
-        <div>
-            <div className="w-14.5 h-18.75 shrink-0 border-rd-1.25 bg-#fff flex" onClick={() => setShowColumnPopup(!showColumnPopup)}>
-                <Image src={currentImage || ''} alt="" width={58} height={75} className="w-14.5 h-18.75 shrink-0 border-rd-1.25"></Image>
-                <div className="mt-3">
-                    <button>
-                        <Image src={"/images/writer/edit/Switch.svg"} alt={""} width={14.09} height={14} className={"w-3.52225 h-3.5 shrink-0 ml-2"}></Image>
-                    </button>
-                    <div className="w-14.08925 text-[#323232] text-3.5 font-not-italic font-400 lh-6 ml-2 mt-2">
-                        {name}
-                    </div>
-                </div>
-            </div>
-            <div className="left-308px top-59px absolute">
-            {showColumnPopup && <ColumnPopup images={images} onImageClick={handleImageClick} />}
-            </div>
-        </div>
-    );
-}
+//     return (
+//         <div>
+//             <div className="w-14.5 h-18.75 shrink-0 border-rd-1.25 bg-#fff flex" onClick={() => setShowColumnPopup(!showColumnPopup)}>
+//                 <Image src={currentImage || ''} alt="" width={58} height={75} className="w-14.5 h-18.75 shrink-0 border-rd-1.25"></Image>
+//                 <div className="mt-3">
+//                     <button>
+//                         <Image src={"/images/writer/edit/Switch.svg"} alt={""} width={14.09} height={14} className={"w-3.52225 h-3.5 shrink-0 ml-2"}></Image>
+//                     </button>
+//                     <div className="w-14.08925 text-[#323232] text-3.5 font-not-italic font-400 lh-6 ml-2 mt-2">
+//                         {name}
+//                     </div>
+//                 </div>
+//             </div>
+//             <div className="left-308px top-59px absolute">
+//             {showColumnPopup && <ColumnPopup images={images} onImageClick={handleImageClick} />}
+//             </div>
+//         </div>
+//     );
+// }
 
-export default Column;
+// export default Column;
+
+
