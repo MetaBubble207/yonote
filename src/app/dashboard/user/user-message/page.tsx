174e608b052ddd6a3d59aa779e2429f09e51ddf9
opacity <2513473008@qq.com>
2024-05-29 23:49:26 +0800

    feat(user-message):新建手机号码修改和验证功能

--- src/app/dashboard/user/user-message/page.tsx
+++ src/app/dashboard/user/user-message/page.tsx
@@ -6,7 +6,11 @@ import {useRef, useState} from "react";
 import OSS from "ali-oss";
 const UserMessage = () => {
     let userInfo;
-    const [token] =   useLocalStorage("token", null);
+    const [token] = useLocalStorage("token", null);
+    const [isEditing, setIsEditing] = useState(false)
+
+    const [editingPhone, setEditingPhone] = useState(userInfo?.phone)
+
     if (token) {
         userInfo = api.users.getOne.useQuery({id:token}).data
     }
@@ -38,7 +42,7 @@ const UserMessage = () => {
         if (file) {
             try {
                 const result = await client.put(file.name, file);
-                updateAvatarApi.mutate({id:token,avatar:result.url})
+                updateAvatarApi.mutate({id: token, avatar: result.url})
                 userInfo.avatar = result.url
             } catch (err) {
                 console.error('Upload error:', err);
@@ -46,15 +50,46 @@ const UserMessage = () => {
         }
     };
 
-    // 处理手机号码
-    const handlePhoneChange = (event)=>{
-        setInput(false)
-        // setPhoneNumber( parseInt(event.target.value));
-        setPhoneNumber(event.target.value.toString())
-        updatePhoneApi.mutate({id:token,phone:phoneNumber})
-        // userInfo.phone = phoneNumber
-        console.log('updating');
+    // 编辑手机号码
+    const handlePhoneButtonClick = () => {
+        setIsEditing(true)
     }
+
+    // 验证手机号码
+    const validatePhoneNumber = (phone) => {
+        //只允许输入数字
+        return /^\d{1,11}$/.test(phone)
+    }
+
+
+    const handlePhoneInputChange = (e) => {
+        const newPhone = e.target.value;
+        setEditingPhone(newPhone);
+    };
+
+
+    // 保存手机号码,可添加手机号码格式验证逻辑
+    const handleSavePhone = () => {
+        if (validatePhoneNumber(editingPhone)&& editingPhone.length===11) {
+            updatePhoneApi.mutate({id: token, phone: editingPhone.toString()})
+            userInfo.phone = editingPhone
+            setEditingPhone(userInfo?.phone)
+            setIsEditing(false)
+        } else {
+            //提示用户手机号码格式错误
+            alert('请输入正确的手机号码')
+        }
+    }
+
+
+    // 取消编辑
+    const handleCancelPhoneEdit = () => {
+        setIsEditing(false)
+        setEditingPhone(userInfo?.phone)
+    }
+
+    //
+
 
     // 触发文件输入框
     const handleButtonClick = () => {
@@ -63,7 +98,6 @@ const UserMessage = () => {
 
     const [input,setInput] = useState(true)
     console.log(userInfo)
-
 
 
     return (
@@ -91,11 +125,38 @@ const UserMessage = () => {
             {/*用户信息*/}
             <div className={"ml-4 mt-11.75"}>
                 <div className={"flex"}><p className={"w-14 text-[#252525] text-3.5 font-500 lh-6"}>用户名</p><span
-                    className={"pl-15 flex-1  text-3.5 font-400"}>{userInfo?.name}</span><Image className={"w-3 h-3 shrink-0 mr-3.25"} src={"/images/user/RightArrow.svg"} alt={"RightArrow"} width={"10"} height={"10"}/></div>
-                <div className={"mt-5.5 flex"}><p className={"w-14 text-[#252525] text-3.5 font-500 lh-6"}>用户ID</p><span className={"pl-15  text-3.5 font-400"}>111</span></div>
+                    className={"pl-15 flex-1  text-3.5 font-400"}>{userInfo?.name}</span><Image
+                    className={"w-3 h-3 shrink-0 mr-3.25"} src={"/images/user/RightArrow.svg"} alt={"RightArrow"}
+                    width={"10"} height={"10"}/></div>
+                <div className={"mt-5.5 flex"}><p className={"w-14 text-[#252525] text-3.5 font-500 lh-6"}>用户ID</p>
+                    <span className={"pl-15  text-3.5 font-400"}>{userInfo?.idNumber}</span></div>
                 <div className={"mt-5.5 flex"}><p className={"w-14 text-[#252525] text-3.5 font-500 lh-6"}>手机号</p>
-                    <input type="text" className='pl-15  text-3.5 font-400' value={userInfo?.phone} onChange={handlePhoneChange} />
-                    <button onClick={handlePhoneChange}>修改</button>
+
+
+                    {isEditing ? (
+                        <>
+
+                            <input type="text" className='pl-15  text-3.5 font-400' value={editingPhone}
+                                   onChange={handlePhoneInputChange}/>
+
+                            <button className='w-10 ml-1.25 text-[#252525] text-2.5 font-500 lh-6'
+                                    onClick={handleSavePhone}>保存
+                            </button>
+                            <button className='w-10 ml-1.25 text-[#252525] text-2.5 font-500 lh-6'
+                                    onClick={handleCancelPhoneEdit}>取消
+                            </button>
+                        </>
+                    ) : (
+                        <div className='flex items-center'>
+                            <span className='flex pl-15  text-3.5 font-400 '>
+                                {userInfo?.phone}</span>
+                            <button className="w-10 ml-1.25 text-[#252525] text-2.5 font-500 lh-6"
+                                    onClick={handlePhoneButtonClick}>
+                                修改
+                            </button>
+                        </div>
+                    )}
+
 
                 </div>
             </div>
