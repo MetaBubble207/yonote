6c067b11e63da1f335c27f1151dc8ea29b900270
xiaban <980892894@qq.com>
2024-05-31 17:45:21 +0800

    feat(column):优化专栏详情页没数据时展示效果

--- src/app/_components/special-column/SpecialColumnList.tsx
+++ src/app/_components/special-column/SpecialColumnList.tsx
@@ -21,17 +21,21 @@ export const SpecialColumnList=()=>{
     const Body = () => {
         return <>
                 <Suspense>
-                    {postInfo && postInfo.length > 0
-                        && postInfo.map((item: any,index) => (
-                            <SpecialColumnCard key={item.id} index={index} item={item} user={user}/>
-                        ))
+                    {
+                        postInfo?.length > 0
+                        ?
+                            postInfo.map((item: any,index) => (
+                                <SpecialColumnCard key={item.id} index={index} item={item} user={user}/>
+                            ))
+                        :
+                            <div className={"text-center mt-30"}>目前暂无数据噢~</div>
                     }
                 </Suspense>
             </>
     }
 
     return(
-        <div>
+        <div className={"min-h-55vh"}>
             <SortLabel/>
             <Body />
         </div>
