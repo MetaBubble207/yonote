c4ee8f89cfe734aa860ceaad88178b552e7dcd8f
opacity <2513473008@qq.com>
2024-06-03 22:37:33 +0800

    fix(next.config.js):清除多余空格

--- next.config.js
+++ next.config.js
@@ -1,34 +1,34 @@
 /**
- * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
- * for Docker builds.
- */
+ * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
+ * for Docker builds.
+ */
 await import("./src/env.js");
 
 /** @type {import("next").NextConfig} */
 const config = {
-    reactStrictMode: false,
-    experimental: {
-        missingSuspenseWithCSRBailout: false,
-      },
-    images: {
-        remotePatterns: [
-            {
-                protocol: 'http',
-                hostname: 'yo-note.oss-cn-shenzhen.aliyuncs.com',
-                port: '',
-            },
-            {
-                protocol: 'https',
-                hostname: 'yo-note.oss-cn-shenzhen.aliyuncs.com',
-                port: '',
-            },
-            {
-                protocol: 'https',
-                hostname: 'thirdwx.qlogo.cn',
-                port: '',
-            }
-        ],
-    },
+    reactStrictMode: false,
+    experimental: {
+        missingSuspenseWithCSRBailout: false,
+      },
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'http',
+                hostname: 'yo-note.oss-cn-shenzhen.aliyuncs.com',
+                port: '',
+            },
+            {
+                protocol: 'https',
+                hostname: 'yo-note.oss-cn-shenzhen.aliyuncs.com',
+                port: '',
+            },
+            {
+                protocol: 'https',
+                hostname: 'thirdwx.qlogo.cn',
+                port: '',
+            }
+        ],
+    },
 };
 
 export default config;
