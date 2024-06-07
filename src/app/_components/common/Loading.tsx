7dc61c422cb6977bb46b17c85dd2d39e62e88517
opacity <2807268479@qq.com>
2024-06-07 14:45:16 +0800

    feat(loading):添加了一个显示加载中的组件

---
+++ src/app/_components/common/Loading.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+const Loading = () => {
+    return (
+        <div
+            className="inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-e-transparent align-[-0.125em] text-surface motion-reduce:animate-[spin_1.5s_linear_infinite] dark:text-white"
+            role="status">
+            <span
+                className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">
+                努力加载中...
+            </span>
+        </div>
+    );
+}
+
+export default Loading;
