47bc53d5cf93fd0c7f510980ddfa3d870640d0fb
opacity <c02cla07@gmail.com>
2024-06-07 22:39:45 +0800

    fix(Loading):样式居中

--- src/app/_components/common/Loading.tsx
+++ src/app/_components/common/Loading.tsx
@@ -1,15 +1,34 @@
+// import React from 'react';
+// const Loading = () => {
+//     return (
+//         <div
+//             className="inline-block ml-40 mt-40 h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-e-transparent align-[-0.125em] text-surface motion-reduce:animate-[spin_1.5s_linear_infinite] dark:text-white"
+//             role="status">
+//             <span
+//                 className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">
+//                 努力加载中...
+//             </span>
+//         </div>
+//     );
+// }
+
+// export default Loading;
 import React from 'react';
+
 const Loading = () => {
     return (
-        <div
-            className="inline-block ml-40 mt-40 h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-e-transparent align-[-0.125em] text-surface motion-reduce:animate-[spin_1.5s_linear_infinite] dark:text-white"
-            role="status">
-            <span
-                className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">
-                努力加载中...
-            </span>
+        <div className="flex justify-center mt-30">
+            <div
+                className="inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-e-transparent align-[-0.125em] text-surface motion-reduce:animate-[spin_1.5s_linear_infinite] dark:text-white"
+                role="status">
+                <span
+                    className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">
+                    努力加载中...
+                </span>
+            </div>
         </div>
     );
 }
 
 export default Loading;
+
