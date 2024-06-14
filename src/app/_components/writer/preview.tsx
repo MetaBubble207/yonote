86355897fee0dc1efc1e39179f22ecdd98bbd4da
opacity <2807268479@qq.com>
2024-06-14 20:36:25 +0800

    fix(preview):修改预览样式

--- src/app/_components/writer/preview.tsx
+++ src/app/_components/writer/preview.tsx
@@ -4,6 +4,9 @@ import TagInput from "../edit/tag";
 import '@wangeditor/editor/dist/css/style.css' // 引入 css
 import { Editor, Toolbar } from '@wangeditor/editor-for-react'
 import { IDomEditor, IEditorConfig, IToolbarConfig, DomEditor } from '@wangeditor/editor'
+import {api} from "@/trpc/react";
+import useLocalStorage from "@/tools/useStore";
+
 
 interface PreviewProps {
     title: string;
@@ -12,6 +15,16 @@ interface PreviewProps {
 }
 
 const Preview = ({ title, html, tags }: PreviewProps) => {
+    const token = useLocalStorage("token", null)
+    const user = api.users.getOne.useQuery({
+        id: token[0],
+    }).data
+    console.log(user)
+    const currentDate = new Date();
+    const year = currentDate.getFullYear();
+    const months = currentDate.getMonth();
+    const days = currentDate.getDate();
+    const specificDate = `${year}-${months}-${days}`;
 
     return (
         <div>
@@ -24,22 +37,22 @@ const Preview = ({ title, html, tags }: PreviewProps) => {
                     {/*左边头像*/}
                     <div className={""}>
                         <div>
-                            <Image src={"/images/special-column/Ellipse 2.png"} alt={"心智与阅读"} width={33} height={33} />
+                            <Image src={user?.avatar} alt={"心智与阅读"} width={33} height={33} />
                         </div>
                     </div>
                     {/*昵称，日期，VIP*/}
                     <div>
                         <div className={"flex items-center"}>
-                            <div className={"text-[#999] text-2.75 font-not-italic font-500 lh-18px ml-5px"}>芋圆</div>
+                            <div className={"text-[#999] text-2.75 font-not-italic font-500 lh-18px ml-5px"}>{user?.name}</div>
                             <div>
                                 <Image src={"/images/special-column/Group 225.png"} alt={"心智与阅读"} width={12} height={12} className={"lh-0"} style={{ marginLeft: "2.5px" }} />
                             </div>
                         </div>
-                        <div className={"text-[#999] text-2.75 font-not-italic font-500 lh-18px ml-5px"}>3.02  12:30发布</div>
+                        <div className={"text-[#999] text-2.75 font-not-italic font-500 lh-18px ml-5px"}>{specificDate}发布</div>
                     </div>
                 </div>
                 <div>
-                    <Image src={"/images/special-column/Rectangle 442.png"} alt={"心智与阅读"} width={343} height={161} className="w-85.75 h-40.25 shrink-0 border-rd-5 m-auto" />
+                    {/*<Image src={"/images/special-column/Rectangle 442.png"} alt={"心智与阅读"} width={343} height={161} className="w-85.75 h-40.25 shrink-0 border-rd-5 m-auto" />*/}
                 </div>
                 <div className={"w-91% mt-24px shrink-0 text-[#666] text-3.5 font-not-italic font-400 lh-[120%] m-auto"}>
                     <div className="h-50 overflow-auto">
