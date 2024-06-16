87b198adeda98a5bde843dc44c06952558bcab22
opacity <2807268479@qq.com>
2024-06-16 22:27:22 +0800

    fix(loading):修改作者端水合报错

--- src/app/_components/common/Loading.tsx
+++ src/app/_components/common/Loading.tsx
@@ -2,12 +2,9 @@ import React from 'react';
 
 const Loading = () => {
     return (
-        <div className="flex justify-center items-center inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-e-transparent align-[-0.125em] text-surface motion-reduce:animate-[spin_1.5s_linear_infinite] dark:text-white"
+        <div className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)] flex justify-center items-center inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-e-transparent align-[-0.125em] text-surface motion-reduce:animate-[spin_1.5s_linear_infinite] dark:text-white"
                 role="status">
-            <span
-                className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">
-                努力加载中...
-            </span>
+            努力加载中...
         </div>
     );
 }
