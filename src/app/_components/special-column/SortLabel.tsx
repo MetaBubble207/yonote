035d26df67b921d1a6d16bea3fd15de086d5119f
opacity <2165768894@qq.com>
2024-06-16 22:46:24 +0800

    fix(column):修复标签字数超出问题

--- src/app/_components/special-column/SortLabel.tsx
+++ src/app/_components/special-column/SortLabel.tsx
@@ -46,7 +46,7 @@ export const SortLabel = (props) => {
 
 
     }
-    const {data:postInfo, isFetched} = api.post.getAllInOrder.useQuery({
+    const { data: postInfo, isFetched } = api.post.getAllInOrder.useQuery({
         columnId: columnId,
         limit: 100000,
         offset: 0,
@@ -55,7 +55,7 @@ export const SortLabel = (props) => {
     const Body = () => {
         return <>
             <Suspense>
-                {!isFetched?<Loading></Loading>:null}
+                {!isFetched ? <Loading></Loading> : null}
                 {postInfo && postInfo.length > 0
                     && postInfo.map((item: any, index) => (
                         <SpecialColumnCard key={item.id} index={index} item={item} user={user} data={status} />
@@ -70,21 +70,21 @@ export const SortLabel = (props) => {
         <div>
             <div className="flex mt-23px overflow-scroll pr-3">
                 <div
-                    className="w-15 h-6 shrink-0 bg-rgba(69,225,184,0.20) ml-24px text-center text-[#1DB48D] text-3.25 font-not-italic font-400 lh-6 border-rd-1"
+                    className="pl-3 pr-3 h-6 shrink-0 bg-rgba(69,225,184,0.20) ml-2 mr-2 text-center text-[#1DB48D] text-3.25 font-not-italic font-400 lh-6 border-rd-1"
                     onClick={() => handleCategoryClick("全部")}
                     style={getCategoryStyle("全部")}
                 >
                     全部
                 </div>
                 <div
-                    className="w-15 h-6 shrink-0 bg-rgba(69,225,184,0.20) ml-24px text-center text-[#1DB48D] text-3.25 font-not-italic font-400 lh-6 border-rd-1"
+                    className="pl-3 pr-3 h-6 shrink-0 bg-rgba(69,225,184,0.20) ml-2 mr-2 text-center text-[#1DB48D] text-3.25 font-not-italic font-400 lh-6 border-rd-1"
                     onClick={() => handleCategoryClick("免费")}
                     style={getCategoryStyle("免费")}
                 >
                     免费
                 </div>
                 <div
-                    className="w-15 h-6 shrink-0 bg-rgba(69,225,184,0.20) ml-24px text-center text-[#1DB48D] text-3.25 font-not-italic font-400 lh-6 border-rd-1"
+                    className="pl-3 pr-3 h-6 shrink-0 bg-rgba(69,225,184,0.20) ml-2 mr-2 text-center text-[#1DB48D] text-3.25 font-not-italic font-400 lh-6 border-rd-1"
                     onClick={() => handleCategoryClick("置顶")}
                     style={getCategoryStyle("置顶")}
                 >
@@ -94,7 +94,7 @@ export const SortLabel = (props) => {
                 {tags?.map((item, index) => (
                     <div
                         key={index}
-                        className="w-15 h-6 shrink-0 bg-rgba(69,225,184,0.20) ml-24px text-center text-[#1DB48D] text-3.25 font-not-italic font-400 lh-6 border-rd-1"
+                        className="h-6 shrink-0 bg-rgba(69,225,184,0.20) ml-2 mr-2 text-center text-[#1DB48D] text-3.25 font-not-italic font-400 lh-6 border-rd-1 pl-3 pr-3"
                         onClick={() => handleCategoryClick(item)}
                         style={getCategoryStyle(item)}
                     >
