87da40e9839b94e3dce4b17efd49509d92a048cd
xiaban <980892894@qq.com>
2024-06-18 04:54:25 +0800

    fix(search):修复如下问题 1、点击专栏推荐——文字没有变黑 2、图片全是模糊的（这个之前我改过成清晰的，肯定是今晚谁又改了） 3、头像应该统一要圆形的 4、卡片文字超出没校验 5、默认情况下，可以拖动页面，右侧会有白边，如果卡片文字超出，这情况更加明显 6、加载数据没有加载中组件的过渡

--- src/app/_components/recommend/SpecialColumn.tsx
+++ src/app/_components/recommend/SpecialColumn.tsx
@@ -16,7 +16,6 @@ export const SpecialColumn = () => {
     };
 
     const [currentContent, setCurrentContent ] = useState<string>("默认");
-    const active = "text-black ";
     const renderContent = () => {
         switch (currentContent){
             case "默认":
@@ -53,11 +52,7 @@ export const SpecialColumn = () => {
 
     return (
         <div>
-            <div className="ml-4 mt-6">
-                <div>
-                    <div className="flex w-full h-6 ">
-                        <div>
-                            <div className="flex">
+            <div className="pl-4 mt-6 flex w-full h-6">
                                 <div
                                     className={`h-6 shrink-0 text-center text-[#1DB48D] text-3.25 font-not-italic font-400 lh-6 border-rd-1 px-9px`}
                                     onClick={() => handleCategoryClick("默认")}
@@ -95,25 +90,7 @@ export const SpecialColumn = () => {
                                     创作时间
                                 </div>
                             </div>
-                        </div>
-                    </div>
-
-                    
-                    {/* <button className="flex">
-                        <div className=" mt-2 text-[#B5B5B5] text-2.5 font-400 lh-6">默认倒序排序</div>  
-                        <Image src={"/images/recommend/sort.svg"} alt={"sort"} width={12} height={12} className="w-3 h-3 mt-3.5 ml-1.25"/>
-                    </button> */}
-                    
-
-                    
-
-
-                </div>
-            </div>
-
             {renderContent()}
-
-
         </div>
             
        
