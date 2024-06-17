e27a26dd86432fcc265b2e570adf37bd0243f1f3
opacity <980892894@qq.com>
2024-06-18 00:11:15 +0800

    style(loading):调整加载组件的样式

--- src/app/_components/common/Loading.tsx
+++ src/app/_components/common/Loading.tsx
@@ -1,8 +1,14 @@
 const Loading = () => {
     return (
-        <div className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)] flex justify-center items-center inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-e-transparent align-[-0.125em] text-surface motion-reduce:animate-[spin_1.5s_linear_infinite] dark:text-white"
+        <div className="flex justify-center items-center ">
+            <div
+                className="inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-e-transparent align-[-0.125em] text-surface motion-reduce:animate-[spin_1.5s_linear_infinite] dark:text-white"
                 role="status">
-            努力加载中...
+                <span
+                    className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">
+                    努力加载中...
+                </span>
+            </div>
         </div>
     );
 }
