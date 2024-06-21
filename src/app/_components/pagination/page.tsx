5a6ad6d542d5aca155bb689d4292f3dcf87a5502
opacity <2513473008@qq.com>
2024-06-21 22:13:58 +0800

    fix(page.tsx): 修改分页组件，添加相关参数

--- src/app/_components/pagination/page.tsx
+++ src/app/_components/pagination/page.tsx
@@ -1,25 +1,33 @@
 'use client'
 import React from 'react';
-import {ConfigProvider, Pagination} from 'antd';
-
+import { ConfigProvider, Pagination } from 'antd';
 import zhCN from 'antd/es/locale/zh_CN';
 
-const MyPagination = () => (
+interface MyPaginationProps {
+    total: number;
+    pageSize: number;
+    current: number;
+    onChange: (page: number, pageSize?: number) => void;
+}
+
+const MyPagination: React.FC<MyPaginationProps> = ({ total, pageSize, current, onChange }) => (
     <ConfigProvider locale={zhCN} theme={{
         components: {
             Pagination: {
-                itemActiveBg:'#DAF9F1',
-                colorPrimary:'#1DB48D',
-                colorPrimaryHover:'#1DB48D',
-                colorText:'rgba(0, 0, 0, 0.65)',
+                itemActiveBg: '#DAF9F1',
+                colorPrimary: '#1DB48D',
+                colorPrimaryHover: '#1DB48D',
+                colorText: 'rgba(0, 0, 0, 0.65)',
             }
         }
     }}>
-
         <Pagination
-            total={85}
+            total={total}
+            pageSize={pageSize}
+            current={current}
             showSizeChanger
             showQuickJumper
+            onChange={onChange}
         />
     </ConfigProvider>
 );
