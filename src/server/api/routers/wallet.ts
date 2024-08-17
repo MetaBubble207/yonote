5ac3872562a4d5b884ce8b72be9ad84e1da26712
xiaban <980892894@qq.com>
2024-08-18 02:42:55 +0800

    feat(wallet): 完成钱包提现功能

--- src/server/api/routers/wallet.ts
+++ src/server/api/routers/wallet.ts
@@ -1,6 +1,6 @@
 import {z} from "zod";
 import {createTRPCRouter, publicProcedure} from "@/server/api/trpc";
-import {wallet} from "@/server/db/schema";
+import {runningWater, wallet} from "@/server/db/schema";
 import {eq} from "drizzle-orm";
 
 
@@ -11,5 +11,20 @@ export const walletRouter = createTRPCRouter({
             return ctx.db.query.wallet.findFirst({
                 where: eq(wallet.userId, input.id)
             });
+        }),
+    withdraw: publicProcedure
+        .input(z.object({id: z.string()}))
+        .mutation(async ({ctx, input}) => {
+            const walletData = await ctx.db.query.wallet.findFirst({where: eq(wallet.userId, input.id)});
+            await ctx.db.update(wallet).set({
+                amountWithdraw: 0
+            });
+            await ctx.db.update(runningWater).set({
+                userId: input.id,
+                price: walletData.amountWithdraw,
+                name: '',
+                expenditureOrIncome: 0
+            })
+            return walletData;
         })
 });
