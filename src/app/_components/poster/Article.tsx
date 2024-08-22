f0c36b97ef83de0c8955417735c987ee7f869d8d
xiaban <980892894@qq.com>
2024-08-22 22:37:39 +0800

    perf: 小部分优化文章分享页

--- src/app/_components/poster/Article.tsx
+++ src/app/_components/poster/Article.tsx
@@ -1,60 +1,62 @@
 "use client"
 import Image from "next/image"
-import {useSearchParams } from "next/navigation";
+import {useSearchParams} from "next/navigation";
 import React, {useRef, useState} from "react";
 import {api} from "@/trpc/react";
 import {useEffect} from "react";
 import useLocalStorage from "@/tools/useStore";
-import  QRCode  from 'qrcode.react';
-import { domToPng } from 'modern-screenshot';
+import QRCode from 'qrcode.react';
+import {domToPng} from 'modern-screenshot';
+import {Button} from "antd";
+import Loading from "@/app/_components/common/Loading";
 
 export const Article = () => {
     const params = useSearchParams();
     const chapter = params ? parseInt(params.get("c")) : null;
     const columnId = params ? params.get("id") : null;
 
-    const { data: postData} = api.post.getById.useQuery({
+    const {data: postData, isLoading: isPostDataLoading} = api.post.getById.useQuery({
         id: columnId,
         chapter: chapter
-      });
-    
-    const userId = api.column.getUserId.useQuery({
-    id: columnId,
     });
 
-    const { data: user} = api.users?.getOne.useQuery({
-    id: userId.data,
+    const userId = api.column.getUserId.useQuery({
+        id: columnId,
     });
-    
+
+    const {data: user, isLoading: isUserLoading} = api.users?.getOne.useQuery({
+        id: userId.data,
+    });
+
     //分享的用户数据
     let userInfo;
     const [token, setToken] = useLocalStorage("token", null);
-    if (typeof window !== "undefined"){
+    if (typeof window !== "undefined") {
         const searchParams = useSearchParams();
         const code = searchParams.get("code");
-        if (code && token === null){
+        if (code && token === null) {
             userInfo = api.users.login.useQuery({
-                code:code
+                code: code
             }).data;
-            if (userInfo){
+            if (userInfo) {
                 setToken(userInfo.id);
             }
         }
         if (token) {
-            userInfo = api.users.getOne.useQuery({ id: token }).data;
+            userInfo = api.users.getOne.useQuery({id: token}).data;
         }
     }
 
     // 点赞数
-    const likeCount = api.like.getLikeCount.useQuery({
+    const {data:likeCount,isLoading:isLikeLoading} = api.like.getLikeCount.useQuery({
         postId: postData?.id || null
-    }).data;
-    
+    });
+
     // 阅读数
-    const readCount = api.read.getPostRead.useQuery({
+    const {data:readCount,isLoading:isReadLoading} = api.read.getPostRead.useQuery({
         postId: postData?.id || null
-    }).data;
-    
+    });
+
     // 文章内容截断处理
     const [postContent, setPostContent] = useState("");
     useEffect(() => {
@@ -69,47 +71,26 @@ export const Article = () => {
     const originURL = window?.location?.origin;
     const qrcodeURL = `${originURL}/special-column-content?c=${chapter}&id=${columnId}&code=${token}`;
 
-    // useRef来获取截图按钮的引用
-    const screenshotButtonRef = useRef(null);
-
+    const png = useRef(null);
     // 处理截图按钮点击事件
     const handleScreenshotClick = async () => {
         try {
-            const dataUrl = await domToPng(document.querySelector('#poster'));
-
+            const dataUrl = await domToPng(png.current);
             // 创建一个<a>元素用于下载截图
-            const link = document.createElement('a');
-            link.download = 'screenshotArticle.png';  
-            link.href = dataUrl; 
-            link.classList.add('hidden'); 
-            console.log(link.href);  //查看生成的图片
-
-            document.body.appendChild(link);
-            link.click();
-            document.body.removeChild(link);
-            alert('保存成功！');
+            const link = document.createElement('a')
+            link.download = 'screenshot.png'
+            link.href = dataUrl
+            link.click()
         } catch (error) {
-            console.error('截图失败:', error);
             alert('无法截图，请重试。');
         }
     };
-
-    // useEffect来监听截图按钮的点击事件
-    useEffect(() => {
-        const button = screenshotButtonRef.current;
-        if (button) {
-            button.addEventListener('click', handleScreenshotClick);
-        }
-        return () => {
-            if (button) {
-                button.removeEventListener('click', handleScreenshotClick);
-            }
-        };
-    }, []); 
+    if (isUserLoading || isPostDataLoading || isReadLoading || isLikeLoading) return <div
+        className={"h-screen w-full flex items-center justify-center"}><Loading/></div>
 
     return (
         <div className="relative min-h-screen bg-[#999999] pt-25.75">
-            <div id="poster" className="w-85.75 h-129.5005 bg-[#ffffff] ml-4">
+            <div ref={png} className="w-85.75 h-129.5005 bg-[#ffffff] ml-4">
                 {/* 顶部作者信息 */}
                 <div className="flex">
                     <div className="flex items-center w-full h-19.375">
@@ -123,11 +104,11 @@ export const Article = () => {
                         />
                         <div className="flex flex-col h-20">
                             <div className="text-[#333333] text-3.5 font-500 lh-6 h-5 ml-2.25 mt-8"
-                                style={{
-                                    overflow: 'hidden',
-                                    textOverflow: 'ellipsis',
-                                    whiteSpace: 'nowrap'
-                                }}>
+                                 style={{
+                                     overflow: 'hidden',
+                                     textOverflow: 'ellipsis',
+                                     whiteSpace: 'nowrap'
+                                 }}>
                                 {user?.name ? user?.name : "未知用户"}
                             </div>
                             {/* 文章点赞数和浏览数 */}
@@ -139,40 +120,44 @@ export const Article = () => {
                             </div>
                         </div>
                     </div>
-                    <Image src={"/images/poster/wholeLogo.svg"} alt="wholeLogo" width={2} height={2} className="w-9.5 h-4.29725 mt-7 mr-5"></Image>
+                    <Image src={"/images/poster/wholeLogo.svg"} alt="wholeLogo" width={2} height={2}
+                           className="w-9.5 h-4.29725 mt-7 mr-5"></Image>
                 </div>
 
                 {/* 文章标题 */}
                 <div className="h-5.387 text-[#333] text-3.5 font-500 lh-6 mt-9 ml-4.5 mx-10"
-                    style={{
-                        overflow: 'hidden',
-                        textOverflow: 'ellipsis',
-                        whiteSpace: 'nowrap'
-                    }}>
+                     style={{
+                         overflow: 'hidden',
+                         textOverflow: 'ellipsis',
+                         whiteSpace: 'nowrap'
+                     }}>
                     {postData?.name ? postData?.name : "暂无数据"}
                 </div>
 
                 {/* 文章内容 */}
                 <div className="relative w-72.462 h-43.25 mt-4 ml-4.5">
-                    <Image src={"/images/poster/title2.svg"} alt="title2" width={2} height={2} className="w-14.75 h-5.25" />
+                    <Image src={"/images/poster/title2.svg"} alt="title2" width={2} height={2}
+                           className="w-14.75 h-5.25"/>
                     <div className="w-72.462 h-35 text-[#666] text-2.5 font-500 lh-6 mt-1.25"
-                        style={{
-                            overflow: 'hidden',
-                            textOverflow: 'ellipsis',
-                            display: '-webkit-box',
-                            WebkitLineClamp: 6,
-                        }}
-                        dangerouslySetInnerHTML={{ __html: postContent }}>
+                         style={{
+                             overflow: 'hidden',
+                             textOverflow: 'ellipsis',
+                             display: '-webkit-box',
+                             WebkitLineClamp: 6,
+                         }}
+                         dangerouslySetInnerHTML={{__html: postContent}}>
                     </div>
                 </div>
 
                 {/* 底部用户信息 */}
                 <div className="flex h-40 ml-4.375 mt-14">
                     <div className="w-40 mt-2">
-                        <button ref={screenshotButtonRef} className="flex items-center w-20 h-3">
-                            <Image src={"/images/poster/triangle.svg"} alt="triangle" width={2} height={2} className="w-2.58125 h-2.58125" />
+                        <Button type={'link'} size={'small'} onClick={handleScreenshotClick}
+                                className="flex items-center w-20 h-3">
+                            <Image src={"/images/poster/triangle.svg"} alt="triangle" width={2} height={2}
+                                   className="w-2.58125 h-2.58125 "/>
                             <div className="h-5.7 text-[#666] text-2.5 font-500 lh-6 ml-1.5">保存到相册</div>
-                        </button>
+                        </Button>
 
                         {/* 用户头像和名称 */}
                         <div className="flex items-center mt-3.5">
@@ -194,12 +179,12 @@ export const Article = () => {
                     </div>
 
                     <div className="ml-18.5">
-                        <div className="w-12 h-12 mx-auto rounded-1.5 border-1.5 border-[#c1c1c1] bg-white">
+                        <div
+                            className="w-16 h-16 mx-auto flex items-center justify-center rounded-1.5 border-1.5 border-[#c1c1c1] ">
                             <QRCode
-                                id="articleQrCode"
+                                id="columnQrCode"
                                 value={qrcodeURL}
-                                size={35}
-                                className="mt-1.25 ml-1.25"
+                                size={50}
                             />
                         </div>
                         <div className="h-5.75 text-[#999] text-2.5 font-400 lh-6 mt-1.25">扫码查看详情</div>
