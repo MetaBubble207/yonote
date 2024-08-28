051debd4ae3e2da3f73e6dfd62f10feecf9b165d
xiaban <980892894@qq.com>
2024-08-28 13:47:16 +0800

    feat: 内容管理页面条件查询接入接口

--- src/app/writer/content-management/page.tsx
+++ src/app/writer/content-management/page.tsx
@@ -1,23 +1,33 @@
 "use client";
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import Link from 'next/link';
 import {useSearchParams} from "next/navigation";
-import {Button, DatePicker} from "antd";
+import {Button, DatePicker, Input} from "antd";
 import {api} from "@/trpc/react";
 import './table.css'
 import TableComponent from "@/app/_components/writer/content/TableComponent";
 import dayjs from "dayjs";
+import 'dayjs/locale/zh-cn';
+import {timeToDateTimeString} from "@/tools/timeToString";
 
 const Page = () => {
     const params = useSearchParams();
     const columnId = params.get("columnId");
-    const {data: posts} = api.post.getAll.useQuery({columnId: columnId}, {enabled: Boolean(columnId)});
-    const [searchParams, setSearchParams] = useState({
+
+    const [queryParams, setQueryParams] = useState({
         title: '',
         tag: '',
-        dateStart: null,
-        dateEnd: null,
+        startDate: null,
+        endDate: null,
     })
+    console.log("searchParams.endDate", queryParams.endDate)
+    const {data: posts, refetch} = api.post.getPostFilter.useQuery({
+        columnId: columnId,
+        title: queryParams.title,
+        tag: queryParams.tag,
+        startDate: queryParams.startDate,
+        endDate: queryParams.endDate,
+    }, {enabled: Boolean(columnId)});
 
     return (
         <div className={'w-full rounded-2.5 bg-[#FFF] pl-8 pr-9'}>
@@ -40,68 +50,89 @@ const Page = () => {
     function ConditionalFiltering() {
         const dateFormat = 'YYYY/MM/DD';
 
+        const [searchParams, setSearchParams] = useState({
+            title: "",
+            tag: "",
+            startDate: null,
+            endDate: null,
+        });
+
+        useEffect(() => {
+            console.log("useEffc=>>", queryParams)
+            setSearchParams({...queryParams});
+        }, []);
+
         const handleSearch = () => {
-            console.log(searchParams);
+            setQueryParams({
+                title: searchParams.title,
+                tag: searchParams.tag,
+                startDate: searchParams.startDate,
+                endDate: searchParams.endDate,
+            });
+            refetch(); // 在点击查询按钮时才触发重新获取数据
         };
 
         const onTitleChange = (e) => {
-            setSearchParams(prev => {
-                prev.title = e.target.value;
-                return prev;
-            })
-        }
+            setSearchParams(prevState => ({...prevState, title: e.target.value}));
+        };
 
         const onTagChange = (e) => {
-            setSearchParams(prev => {
-                prev.tag = e.target.value;
-                return prev;
-            })
-        }
+            setSearchParams(prevState => ({...prevState, tag: e.target.value}));
+        };
 
         const onDateChange = (date) => {
             const [date1, date2] = date;
-            setSearchParams(prev => {
-                prev.dateStart = new Date(date1.$d)
-                prev.dateEnd = new Date(date2.$d)
-                return prev;
-            })
+            setSearchParams((prev) => ({
+                ...prev,
+                startDate: date1 ? date1.toDate() : null,
+                endDate: date2 ? date2.toDate() : null,
+            }));
         }
 
         const disabledDate = (currentDate, info) => {
             return currentDate.isAfter(dayjs(), 'day');
         };
+
         return (
-            <div className={'flex mt-30px'}>
+            <div className={'flex mt-7.5 text-3.5 font-400'}>
                 <div>
-                    <label className='text-[rgba(0,0,0,0.65)] text-3.5 font-400 lh-5.5'>内容标题: </label>
-                    <input
-                        className='ml-4 w-56 h-8 pl-3.045 shrink-0 border-rd-1 border-1 border-solid border-[#D9D9D9] bg-[#FFF] lh-8'
+                    <label className='lh-5.5'>内容标题：</label>
+                    <Input
+                        className='ml-4 w-56 h-8 rounded-1 border-1 border-solid border-[#D9D9D9] bg-[#FFF]'
                         type="text"
                         placeholder="搜索标题"
-                        onBlur={onTitleChange}
+                        value={searchParams.title}
+                        onChange={onTitleChange}
+                        allowClear
                     />
                 </div>
 
                 <div className={'ml-32px'}>
-                    <label className='text-[rgba(0,0,0,0.65)] text-3.5 font-400 lh-5.5'>标签: </label>
-                    <input
-                        className='ml-4 w-56 h-8 pl-3.045 shrink-0 border-rd-1 border-1 border-solid border-[#D9D9D9] bg-[#FFF] lh-8'
+                    <label className='lh-5.5'>标签：</label>
+                    <Input
+                        className='ml-4 w-56 h-8 rounded-1 border-1 border-solid border-[#D9D9D9] bg-[#FFF]'
                         type="text"
                         placeholder="不限"
-                        onBlur={onTagChange}
+                        value={searchParams.tag}
+                        onChange={onTagChange}
+                        allowClear
                     />
                 </div>
                 <div className={"flex items-center ml-32px"}>
-                    <label className='text-[rgba(0,0,0,0.65)] text-3.5 font-400 lh-5.5 mr-10px'>发布时间: </label>
-                    {/*<Date></Date>*/}
+                    <label className='text-3.5 font-400 lh-5.5 mr-2.5'>发布时间：</label>
                     <DatePicker.RangePicker
+                        allowClear
+                        value={[
+                            searchParams.startDate ? dayjs(searchParams.startDate) : null,
+                            searchParams.endDate ? dayjs(searchParams.endDate) : null,
+                        ]}
                         format={dateFormat}
                         onChange={onDateChange}
                         disabledDate={(currentDate, info) => disabledDate(currentDate, info)}
                     />
                 </div>
                 <Button
-                    className={'p0 b-0 h-32px border-rd-1 bg-[#1DB48D] text-[#fff] px-16px font-400 text-3.5 lh-32px mx-56px'}
+                    className={'p0 b-0 w-16 h-8 rounded-1 bg-[#1DB48D] text-[#fff] mx-56px'}
                     onClick={handleSearch}
                 >
                     查询
