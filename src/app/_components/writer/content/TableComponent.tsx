8cf8aefd5d29997c25136535585c1ac5e910e5ee
xiaban <980892894@qq.com>
2024-08-28 10:48:10 +0800

    refactor: 文章内容管理重构

---
+++ src/app/_components/writer/content/TableComponent.tsx
@@ -0,0 +1,150 @@
+import {type Post} from "@/server/db/schema";
+import {Button, Table, type TableColumnsType, type TableProps} from "antd";
+import React from "react";
+import {useRouter} from "next/navigation";
+import {timeToDateTimeString} from "@/tools/timeToString";
+import Link from "next/link";
+
+const TableComponent = ({dataSource}: { dataSource: Post[] }) => {
+    const router = useRouter()
+    const handleEdit = (postId) => {
+        // 编辑文章逻辑
+        router.push(`/edit?postId=${postId}`);
+    };
+
+    const handleToggleTop = (index: number) => {
+
+    }
+
+    const handleToggleFree = (index: number) => {
+
+    }
+
+    const handleClickDelete = (id: number) => {
+
+    }
+    const columns: TableColumnsType<Post> = [
+        {
+            title: <span className={'pl-20'}>内容标题</span>,
+            dataIndex: 'name',
+            sorter: (a, b) => a.name.localeCompare(b.name),
+            render: (value) => <span className={'pl-20'}>{value}</span>,
+            width: '25%'
+        },
+        {
+            title: '状态',
+            dataIndex: 'status',
+            sorter: (a, b) => {
+                // 定义排序逻辑
+                const isTopA = a.isTop;
+                const isTopB = b.isTop;
+                const isFreeA = a.isFree;
+                const isFreeB = b.isFree;
+
+                // 如果置顶状态不同，则按置顶状态排序
+                if (isTopA !== isTopB) {
+                    return isTopA ? -1 : 1;
+                }
+
+                // 如果置顶状态相同，则按免费状态排序
+                if (isFreeA !== isFreeB) {
+                    return isFreeA ? -1 : 1;
+                }
+
+                // 如果置顶和免费状态都相同，则按其他条件排序（例如按 id 排序）
+                return a.id - b.id;
+            },
+            filters: [
+                {
+                    text: '免费',
+                    value: 'free',
+                },
+                {
+                    text: '置顶',
+                    value: 'top'
+                }
+            ],
+            onFilter: (value, record) => {
+                const {isTop, isFree} = record;
+                if (value === 'free') {
+                    return isFree;
+                }
+                if (value === 'top') {
+                    return isTop;
+                }
+                return false;
+            },
+            render: (status, record) => {
+                const {isTop, isFree} = record;
+                return (
+                    <div className={"flex space-x-2.5 items-center"}>
+                        {isTop && <div className={"flex items-center"}>
+                            <span className={'w-6px h-6px rounded-full p0 bg-#1DB48D flex'}></span>
+                            <span className={'text-14px ml-2'}>置顶</span>
+                        </div>}
+                        {isFree && <div className={"flex items-center"}>
+                            <span className={'w-6px h-6px rounded-full p0 bg-#FDB069 flex'}></span>
+                            <span className={'text-14px ml-2'}>免费</span>
+                        </div>}
+                    </div>
+                );
+            },
+            width: '12.5%',
+        },
+        {
+            title: '标签',
+            dataIndex: 'tag',
+            sorter: (a, b) => {
+                // 将标签字符串分割成数组，并计算长度
+                const tagLengthA = a.tag.split(',').length;
+                const tagLengthB = b.tag.split(',').length;
+                // 根据长度进行排序
+                return tagLengthA - tagLengthB;
+            },
+            render: (tags) => tags.split(',').map((tag: string, index: number) => <span key={index}>{tag}</span>),
+            width: '9%',
+        },
+        {
+            title: '更新时间',
+            dataIndex: 'updatedAt',
+            sorter: (a, b) => a.updatedAt > b.updatedAt ? 1 : -1,
+            filterSearch: true,
+            render: (value) => <span>{timeToDateTimeString(value)}</span>,
+            width: '15.5%',
+        },
+        {
+            title: '发布时间',
+            dataIndex: 'createdAt',
+            sorter: (a, b) => a.createdAt > b.createdAt ? 1 : -1,
+            filterSearch: true,
+            render: (value) => <span>{timeToDateTimeString(value)}</span>,
+            width: '15.5%',
+        },
+        {
+            title: '操作',
+            render: (_, record, index) => (
+                <div className={'text-3.5 font-400 lh-5.5 text-[#1DB48D] space-x-2'}>
+                    <Link href={`/edit/edit?columnId=${record.columnId}&postId=${record.id}`}
+                          onClick={handleEdit}>编辑</Link>
+                    <Button type={'link'} className="w-14 text-[#1DB48D]"
+                            onClick={() => handleToggleTop(index)}>{record.isTop ? '取消置顶' : '置顶'}</Button>
+                    <Button type={'link'} className="w-14 text-[#1DB48D]"
+                            onClick={() => handleToggleFree(index)}>{record.isFree ? '取消免费' : '免费'} </Button>
+                    <Button type={'link'} className={'text-[#1DB48D]'}
+                            onClick={() => handleClickDelete(record.id)}>删除</Button>
+                </div>
+            ),
+            width: '24%',
+        },
+    ];
+    const onChange: TableProps<Post>['onChange'] = (pagination, filters, sorter, extra) => {
+        console.log('params', pagination, filters, sorter, extra);
+    };
+    return (
+        <Table columns={columns} onChange={onChange}
+               dataSource={dataSource} pagination={{position: ['bottomCenter']}}
+               rowKey={(record) => record.id}/>
+    )
+}
+
+export default TableComponent;
--- src/app/writer/content-management/page.tsx
+++ src/app/writer/content-management/page.tsx
@@ -1,163 +1,24 @@
 "use client";
-import React, {useRef, useState} from 'react';
-import Date from "@/app/_components/writer/datarange/Date";
-import ContentForm from "@/app/_components/writer/ContentForm";
+import React, {useState} from 'react';
 import Link from 'next/link';
-import {useRouter, useSearchParams} from "next/navigation";
-import {Button, Table, type TableColumnsType, type TableProps} from "antd";
+import {useSearchParams} from "next/navigation";
+import {Button, DatePicker} from "antd";
 import {api} from "@/trpc/react";
-import {type Post} from "@/server/db/schema";
-import {timeToDateTimeString} from "@/tools/timeToString";
 import './table.css'
+import TableComponent from "@/app/_components/writer/content/TableComponent";
+import dayjs from "dayjs";
 
 const Page = () => {
-    const router = useRouter()
-    const handleEdit = (postId) => {
-        // 编辑文章逻辑
-        router.push(`/edit?postId=${postId}`);
-    };
-
-    const handleToggleTop = (index: number) => {
-
-    }
-
-    const handleToggleFree = (index: number) => {
-
-    }
-
-    const handleClickDelete = (id: number) => {
-
-    }
-    const columns: TableColumnsType<Post> = [
-        {
-            title: <span className={'pl-20'}>内容标题</span>,
-            dataIndex: 'name',
-            sorter: (a, b) => a.name.localeCompare(b.name),
-            render: (value) => <span className={'pl-20'}>{value}</span>
-        },
-        {
-            title: '状态',
-            dataIndex: 'status',
-            sorter: (a, b) => {
-                // 定义排序逻辑
-                const isTopA = a.isTop;
-                const isTopB = b.isTop;
-                const isFreeA = a.isFree;
-                const isFreeB = b.isFree;
-
-                // 如果置顶状态不同，则按置顶状态排序
-                if (isTopA !== isTopB) {
-                    return isTopA ? -1 : 1;
-                }
-
-                // 如果置顶状态相同，则按免费状态排序
-                if (isFreeA !== isFreeB) {
-                    return isFreeA ? -1 : 1;
-                }
-
-                // 如果置顶和免费状态都相同，则按其他条件排序（例如按 id 排序）
-                return a.id - b.id;
-            },
-            filters: [
-                {
-                    text: '免费',
-                    value: 'free',
-                },
-                {
-                    text: '置顶',
-                    value: 'top'
-                }
-            ],
-            onFilter: (value, record) => {
-                const {isTop, isFree} = record;
-                if (value === 'free') {
-                    return isFree;
-                }
-                if (value === 'top') {
-                    return isTop;
-                }
-                return false;
-            },
-            render: (status, record) => {
-                const {isTop, isFree} = record;
-                return (
-                    <div className={"flex space-x-2.5 items-center"}>
-                        {isTop && <div className={"flex items-center"}>
-                            <span className={'w-6px h-6px rounded-full p0 bg-#1DB48D flex'}></span>
-                            <span className={'text-14px ml-2'}>置顶</span>
-                        </div>}
-                        {isFree && <div className={"flex items-center"}>
-                            <span className={'w-6px h-6px rounded-full p0 bg-#FDB069 flex'}></span>
-                            <span className={'text-14px ml-2'}>免费</span>
-                        </div>}
-                    </div>
-                );
-            },
-        },
-        {
-            title: '标签',
-            dataIndex: 'tag',
-            sorter: (a, b) => {
-                // 将标签字符串分割成数组，并计算长度
-                const tagLengthA = a.tag.split(',').length;
-                const tagLengthB = b.tag.split(',').length;
-                // 根据长度进行排序
-                return tagLengthA - tagLengthB;
-            },
-            render: (tags) => tags.split(',').map((tag: string, index: number) => <span key={index}>{tag}</span>),
-        },
-        {
-            title: '更新时间',
-            dataIndex: 'updatedAt',
-            sorter: (a, b) => a.updatedAt > b.updatedAt ? 1 : -1,
-            filterSearch: true,
-            render: (value) => <span>{timeToDateTimeString(value)}</span>
-        },
-        {
-            title: '发布时间',
-            dataIndex: 'createdAt',
-            sorter: (a, b) => a.createdAt > b.createdAt ? 1 : -1,
-            filterSearch: true,
-            render: (value) => <span>{timeToDateTimeString(value)}</span>
-        },
-        {
-            title: '操作',
-            render: (_, record, index) => (
-                <div className={'text-3.5 font-400 lh-5.5 text-[#1DB48D] space-x-2'}>
-                    <Link href={`/edit/edit?columnId=${columnId}&postId=${record.id}`}
-                          onClick={handleEdit}>编辑</Link>
-                    <Button type={'link'} className="w-14 text-[#1DB48D]"
-                            onClick={() => handleToggleTop(index)}>{record.isTop ? '取消置顶' : '置顶'}</Button>
-                    <Button type={'link'} className="w-14 text-[#1DB48D]"
-                            onClick={() => handleToggleFree(index)}>{record.isFree ? '取消免费' : '免费'} </Button>
-                    <Button type={'link'} className={'text-[#1DB48D]'}
-                            onClick={() => handleClickDelete(record.id)}>删除</Button>
-                </div>
-            ),
-        },
-    ];
-
     const params = useSearchParams();
     const columnId = params.get("columnId");
+    const {data: posts} = api.post.getAll.useQuery({columnId: columnId}, {enabled: Boolean(columnId)});
+    const [searchParams, setSearchParams] = useState({
+        title: '',
+        tag: '',
+        dateStart: null,
+        dateEnd: null,
+    })
 
-    const {data: posts} = api.post.getAll.useQuery({
-        limit: 5,
-        offset: 0,
-        columnId: columnId,
-    });
-
-    const titleRef = useRef(null);
-    const tagRef = useRef(null);
-
-    const handleSearch = () => {
-        const titleValue = titleRef.current.value;
-        const tagValue = tagRef.current.value;
-        console.log("内容标题:", titleValue);
-        console.log("标签:", tagValue);
-    };
-    const onChange: TableProps<Post>['onChange'] = (pagination, filters, sorter, extra) => {
-        console.log('params', pagination, filters, sorter, extra);
-    };
     return (
         <div className={'w-full rounded-2.5 bg-[#FFF] pl-8 pr-9'}>
             <div className={'flex items-center pt-51px'}>
@@ -167,8 +28,49 @@ const Page = () => {
                       className={'inline-block h-32px border-rd-1 bg-[rgba(69,225,184,0.20)] text-[#1db48d] px-16px lh-32px ml-32px'}>+
                     发布</Link>
             </div>
+            <ConditionalFiltering/>
+            {/*表格*/}
+            <div className={"mt-4"}>
+                {/*@ts-ignore*/}
+                <TableComponent dataSource={posts}/>
+            </div>
+        </div>
+    );
 
-            {/*justify-between*/}
+    function ConditionalFiltering() {
+        const dateFormat = 'YYYY/MM/DD';
+
+        const handleSearch = () => {
+            console.log(searchParams);
+        };
+
+        const onTitleChange = (e) => {
+            setSearchParams(prev => {
+                prev.title = e.target.value;
+                return prev;
+            })
+        }
+
+        const onTagChange = (e) => {
+            setSearchParams(prev => {
+                prev.tag = e.target.value;
+                return prev;
+            })
+        }
+
+        const onDateChange = (date) => {
+            const [date1, date2] = date;
+            setSearchParams(prev => {
+                prev.dateStart = new Date(date1.$d)
+                prev.dateEnd = new Date(date2.$d)
+                return prev;
+            })
+        }
+
+        const disabledDate = (currentDate, info) => {
+            return currentDate.isAfter(dayjs(), 'day');
+        };
+        return (
             <div className={'flex mt-30px'}>
                 <div>
                     <label className='text-[rgba(0,0,0,0.65)] text-3.5 font-400 lh-5.5'>内容标题: </label>
@@ -176,7 +78,7 @@ const Page = () => {
                         className='ml-4 w-56 h-8 pl-3.045 shrink-0 border-rd-1 border-1 border-solid border-[#D9D9D9] bg-[#FFF] lh-8'
                         type="text"
                         placeholder="搜索标题"
-                        ref={titleRef}
+                        onBlur={onTitleChange}
                     />
                 </div>
 
@@ -186,12 +88,17 @@ const Page = () => {
                         className='ml-4 w-56 h-8 pl-3.045 shrink-0 border-rd-1 border-1 border-solid border-[#D9D9D9] bg-[#FFF] lh-8'
                         type="text"
                         placeholder="不限"
-                        ref={tagRef}
+                        onBlur={onTagChange}
                     />
                 </div>
                 <div className={"flex items-center ml-32px"}>
                     <label className='text-[rgba(0,0,0,0.65)] text-3.5 font-400 lh-5.5 mr-10px'>发布时间: </label>
-                    <Date></Date>
+                    {/*<Date></Date>*/}
+                    <DatePicker.RangePicker
+                        format={dateFormat}
+                        onChange={onDateChange}
+                        disabledDate={(currentDate, info) => disabledDate(currentDate, info)}
+                    />
                 </div>
                 <Button
                     className={'p0 b-0 h-32px border-rd-1 bg-[#1DB48D] text-[#fff] px-16px font-400 text-3.5 lh-32px mx-56px'}
@@ -200,17 +107,8 @@ const Page = () => {
                     查询
                 </Button>
             </div>
-
-            {/*表格*/}
-            <div className={"mt-4"}>
-                {/* @ts-ignore*/}
-                <Table columns={columns} onChange={onChange}
-                       dataSource={posts} pagination={{position: ['bottomCenter']}}
-                       rowKey={(record) => record.id}/>
-            </div>
-            {/*底部分页*/}
-        </div>
-    );
+        )
+    }
 };
 
 export default Page;
