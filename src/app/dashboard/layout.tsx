eaaf34d991d41886a8f15e4b5f0433240852908b
黄希 <huangxi@eslab.cn>
2024-11-13 20:53:34 +0800

    style: 调整用户端备案模块位置

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -5,7 +5,7 @@ export default function Dashboard({children}: { children: React.ReactNode }) {
     return <>
         <div>{children}</div>
         <div
-            className={'z-1 absolute bottom-10 flex items-center justify-center text-gray bg-transparent text-3 w-100vw'}>
+            className={'z-1 h-40 flex items-center justify-center text-gray bg-#F5F7FB text-3 w-100vw'}>
             ICP备案号：
             <Link href={'http://beian.miit.gov.cn/ '}> 京ICP备2024064381号-1</Link>
         </div>
