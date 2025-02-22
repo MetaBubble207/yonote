c165836d451559c924188099741047454dc180f6
xiaban <980892894@qq.com>
2025-02-22 18:55:47 +0800

    fix: 修复server component的searchparams的错误运用

--- src/app/writer/homepage/page.tsx
+++ src/app/writer/homepage/page.tsx
@@ -1,25 +1,17 @@
-"use client";
 import React from "react";
 import HomepageData from "@/app/_components/writer/homepage/HomepageData";
 import Chart from "@/app/_components/writer/homepage/Chart";
 
-const Page = ({
-  params,
-  searchParams,
-}: {
-  params: { slug: string };
-  searchParams: { columnId: string | undefined };
-}) => {
+export default async function Page({ searchParams }: { searchParams: Promise<{ columnId: string }> }) {
+  const columnId = (await searchParams).columnId;
   return (
     <div className="h-full w-full">
       <div className="w-full">
-        <HomepageData columnId={searchParams?.columnId} />
+        <HomepageData columnId={columnId} />
       </div>
       <div className="w-full pt-4">
-        <Chart columnId={searchParams?.columnId} />
+        <Chart columnId={columnId} />
       </div>
     </div>
   );
 };
-
-export default Page;
