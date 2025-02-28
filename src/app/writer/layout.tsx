fc08b4264af4f94b44f7d41b8a56b22008697fb9
xiaban <980892894@qq.com>
2025-02-28 09:47:53 +0800

    style: 调整作者端 layout 样式

--- src/app/writer/layout.tsx
+++ src/app/writer/layout.tsx
@@ -8,37 +8,20 @@ import { usePathname } from "next/navigation";
 import Link from "next/link";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
-  const pathname = usePathname();
-  const filteredPath = ["/writer/login", "/writer/no-column"];
   return (
     <div className="min-h-screen w-full bg-[#F6F6F6]">
       <Compass></Compass>
-      <div className="w-100% flex">
+      <div className="w-full flex">
         <LeftCompass></LeftCompass>
-        {withTheme(
-          <div
-            className={`relative w-full ${
-              !filteredPath.includes(pathname)
-                ? "min-h-235 pl-69.12 pt-21.5 pb-4 pr-8"
-                : "h-100vh"
-            }`}
-          >
-            <div>
-              {children}
-              <div
-                className={
-                  "z-1 left-50% text-gray text-3 absolute bottom-5 flex items-center justify-center bg-transparent"
-                }
-              >
-                ICP备案号：
-                <Link href={"http://beian.miit.gov.cn/ "}>
-                  {" "}
-                  京ICP备2024064381号-1
-                </Link>
-              </div>
-            </div>
-          </div>,
-        )}
+        <div className={`w-full flex flex-col flex-1 pl-70 pt-21.4 pr-4`}>
+          <div className="flex-1">{children}</div>
+          <div className={"text-gray text-3 flex items-center justify-center bg-transparent mt-8"}>
+            ICP备案号：
+            <Link href={"http://beian.miit.gov.cn/ "}>
+              京ICP备2024064381号-1
+            </Link>
+          </div>
+        </div>,
       </div>
     </div>
   );
