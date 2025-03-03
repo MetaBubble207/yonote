1865179ac5ff98b45db1c231b606972040bd5e57
xiaban <980892894@qq.com>
2025-03-03 20:43:25 +0800

    perf: 优化通用骨架屏组件细节，添加一个y 间距的参数

--- src/app/_components/common/LoadingSkeleton.tsx
+++ src/app/_components/common/LoadingSkeleton.tsx
@@ -3,10 +3,11 @@ import { Skeleton } from "antd";
 interface LoadingSkeletonProps {
     rows?: number;
     count?: number;
+    spaceY?: number;
     className?: string;
 }
 
-export const LoadingSkeleton = ({ rows = 5, count = 3, className }: LoadingSkeletonProps) => (
+export const LoadingSkeleton = ({ rows = 5, count = 3, spaceY = 2, className }: LoadingSkeletonProps) => (
     <div className={className}>
         {Array.from({ length: count }).map((_, index) => (
             <Skeleton
@@ -14,7 +15,7 @@ export const LoadingSkeleton = ({ rows = 5, count = 3, className }: LoadingSkele
                 active
                 paragraph={{ rows }}
                 title={false}
-                className={`w-85.75 h-${rows*10} border-rd-5 p4 mb-2 bg-[#FFF]`}
+                className={`w-85.75 h-${rows*8} border-rd-5 p4 mb-${spaceY} bg-[#FFF]`}
             />
         ))}
     </div>
