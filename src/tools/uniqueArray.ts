1f2e8d92e61423fafe413f2ba860f9ccf4a834db
xiaban <980892894@qq.com>
2025-03-03 17:31:36 +0800

    perf: 优化数组去重方法

--- src/tools/uniqueArray.ts
+++ src/tools/uniqueArray.ts
@@ -1,5 +1,14 @@
-export const uniqueArray = (arr, key) => {
-  return arr.reduce((acc, item) => {
+/**
+ * 根据指定的键去除数组中的重复项
+ * @param arr 需要去重的数组
+ * @param key 用于判断重复的键名
+ * @returns 去重后的数组
+ */
+export const uniqueArray = <T extends Record<K, any>, K extends keyof T>(
+  arr: T[],
+  key: K
+): T[] => {
+  return arr.reduce((acc: T[], item: T) => {
     if (!acc.find((i) => i[key] === item[key])) {
       acc.push(item);
     }
