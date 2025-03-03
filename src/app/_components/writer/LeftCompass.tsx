b7eaed2c90a7b7c994869307a07926ddd03acfc1
xiaban <980892894@qq.com>
2025-03-03 11:03:30 +0800

    refactor: 重构侧边栏组件，图标规范化

--- src/app/_components/writer/LeftCompass.tsx
+++ src/app/_components/writer/LeftCompass.tsx
@@ -8,6 +8,49 @@ import ColumnList from "./ColumnList";
 import { api } from "@/trpc/react";
 import useLocalStorage from "@/app/_hooks/useLocalStorage";
 
+// 统一图标路径配置
+const ICON_BASE = "/images/writer/compass/";
+const ACTIVE_ICON_SUFFIX = "-active.svg";
+const INACTIVE_ICON_SUFFIX = "-unactive.svg";
+type NavConfig = {
+  path: string;
+  icon: string;
+  label: string;
+};
+// 导航项配置表
+const navConfig: NavConfig[] = [
+  {
+    path: "/homepage",
+    icon: "homepage",
+    label: "主页看板"
+  },
+  {
+    path: "/content-management",
+    icon: "content",
+    label: "内容管理"
+  },
+  {
+    path: "/subscribe-manage",
+    icon: "subscribe",
+    label: "订阅管理"
+  },
+  {
+    path: "/column-settings",
+    icon: "settings",
+    label: "专栏设置"
+  },
+  {
+    path: "/co-author",
+    icon: "co-author",
+    label: "共创作者"
+  },
+  {
+    path: "/speed-up",
+    icon: "speed-up",
+    label: "加速计划"
+  }
+];
+
 const LeftCompass = () => {
   const pathname = usePathname();
   const searchParams = useSearchParams();
@@ -16,166 +59,62 @@ const LeftCompass = () => {
   const [token] = useLocalStorage('token', null);
   const { data: latestColumnId } = api.column.getLatestColumnId.useQuery(token, { enabled: Boolean((columnId === undefined || columnId === null) && token) });
   const [columnIdState, setColumnIdState] = useState<string>();
+
+  // 组件初始化逻辑
   useEffect(() => {
     if (!columnId || columnId === null) {
       if (latestColumnId) {
-        console.log("latest =++++>", latestColumnId)
         setColumnIdState(latestColumnId)
         router.push(`${pathname}?columnId=${latestColumnId}`)
-      } else {
-        setColumnIdState(columnId!)
       }
+    } else {
+      setColumnIdState(columnId)
     }
     if (!token) {
       router.push('/writer/login')
     }
   }, [columnId, token, latestColumnId])
+
+  // 动态生成导航项
+  const renderNavItem = (item: NavConfig) => {
+    const isActive = pathname === `/writer${item.path}`;
+    const iconSrc = isActive
+      ? `${ICON_BASE}${item.icon}${ACTIVE_ICON_SUFFIX}`
+      : `${ICON_BASE}${item.icon}${INACTIVE_ICON_SUFFIX}`;
+    console.log("item ==>", item)
+    return (
+      <div
+        key={item.path}
+        className={`w-65.25 h-12 flex items-center shrink-0 ${isActive ? "bg-[#DAF9F1] text-[#4CC5A6]" : "bg-[#FFF] text-[#999]"
+          }`}
+      >
+        <Image
+          src={iconSrc}
+          alt={item.icon}
+          width={item.icon === "content" ? 18 : 24}
+          height={item.icon === "content" ? 18 : 24}
+          className={`shrink-0 ${item.icon === "content"? "ml-11.75" : "ml-11"}`}
+        />
+        <Link href={`/writer/${item.path}?columnId=${columnIdState}`} passHref>
+          <div className={`text-3.5 font-400 lh-6 w-14 shrink-0 ${item.icon === "content"? "ml-3" : "ml-2"}`}>
+            {item.label}
+          </div>
+        </Link>
+      </div>
+    );
+  };
+
   return (
     <>
       <div className="top-17.5 w-65 z-100 fixed h-screen shrink-0 bg-[#FFF]">
         <div className="w-65 h-72.4785 mt-10 flex shrink-0 flex-col">
           <ColumnList />
           <div
-            className={`w-65.25 h-12.4705 mt-35px flex shrink-0 ${pathname === "/writer/homepage"
-              ? "bg-[#DAF9F1] text-[#4CC5A6]"
-              : "bg-[#FFF] text-[#999]"
-              }`}
+            className={`w-65.25 h-12.4705 mt-35px flex shrink-0 ${pathname === "/writer/homepage" ? "bg-[#DAF9F1] text-[#4CC5A6]" : ""}`}
           >
-            <Image
-              src={`${pathname === "/writer/homepage"
-                ? "/images/writer/edit/Monitor1.svg"
-                : "/images/writer/edit/Monitor-one.svg"
-                }`}
-              alt="Monitor1"
-              width={24}
-              height={24.03}
-              className={`h-6.00625 ml-10.555 mt-2.89 w-6 shrink-0`}
-            ></Image>
-            <Link href={`/writer/homepage?columnId=${columnId}`} passHref>
-              <div className="h-6.00625 text-3.5 font-400 lh-6 ml-2.07 mt-2.89 w-14 shrink-0">
-                主页看板
-              </div>
-            </Link>
+            {renderNavItem(navConfig[0]!)}
           </div>
-          <div
-            className={`w-65.25 h-12.4705 flex shrink-0 ${pathname === "/writer/content-management"
-              ? "bg-[#DAF9F1] text-[#4CC5A6]"
-              : "bg-[#FFF] text-[#999]"
-              }`}
-          >
-            <Image
-              src={`${pathname === "/writer/content-management"
-                ? "/images/writer/edit/Rocket2 copy.svg"
-                : "/images/writer/edit/Rocket2.svg"
-                }`}
-              alt="Monitor1"
-              width={24}
-              height={24.03}
-              className="h-6.00625 ml-10.555 mt-2.89 w-6 shrink-0"
-            ></Image>
-            <Link
-              href={`/writer/content-management?columnId=${columnIdState}`}
-              passHref
-            >
-              <div className="h-6.00625 text-3.5 font-400 lh-6 ml-2.07 mt-2.89 w-14 shrink-0">
-                内容管理
-              </div>
-            </Link>
-          </div>
-          <div
-            className={`w-65.25 h-12.4705 flex shrink-0 ${pathname === "/writer/subscribe-manage"
-              ? "bg-[#DAF9F1] text-[#4CC5A6]"
-              : "bg-[#FFF] text-[#999]"
-              }`}
-          >
-            <Image
-              src={`${pathname === "/writer/subscribe-manage"
-                ? "/images/writer/edit/Rocket3 copy.svg"
-                : "/images/writer/edit/Rocket3.svg"
-                }`}
-              alt="Monitor1"
-              width={24}
-              height={24.03}
-              className="h-6.00625 ml-10.555 mt-2.89 w-6 shrink-0"
-            ></Image>
-            <Link
-              href={`/writer/subscribe-manage?columnId=${columnIdState}`}
-              passHref
-            >
-              <div className="h-6.00625 text-3.5 font-400 lh-6 ml-2.07 mt-2.89 w-14 shrink-0">
-                订阅管理
-              </div>
-            </Link>
-          </div>
-          <div
-            className={`w-65.25 h-12.4705 flex shrink-0 ${pathname === "/writer/column-settings"
-              ? "bg-[#DAF9F1] text-[#4CC5A6]"
-              : "bg-[#FFF] text-[#999]"
-              }`}
-          >
-            <Image
-              src={`${pathname === "/writer/column-settings"
-                ? "/images/writer/edit/Rocket4 copy.svg"
-                : "/images/writer/edit/Rocket4.svg"
-                }`}
-              alt="Monitor1"
-              width={24}
-              height={24.03}
-              className="h-6.00625 ml-10.555 mt-2.89 w-6 shrink-0"
-            ></Image>
-            <Link
-              href={`/writer/column-settings?columnId=${columnIdState}`}
-              passHref
-            >
-              <div className="h-6.00625 text-3.5 font-400 lh-6 ml-2.07 mt-2.89 w-14 shrink-0">
-                专栏设置
-              </div>
-            </Link>
-          </div>
-          <div
-            className={`w-65.25 h-12.4705 flex shrink-0 ${pathname === "/writer/co-author"
-              ? "bg-[#DAF9F1] text-[#4CC5A6]"
-              : "bg-[#FFF] text-[#999]"
-              }`}
-          >
-            <Image
-              src={`${pathname === "/writer/co-author"
-                ? "/images/writer/edit/Rocket5 copy.svg"
-                : "/images/writer/edit/Rocket5.svg"
-                }`}
-              alt="Monitor1"
-              width={24}
-              height={24.03}
-              className="h-6.00625 ml-10.555 mt-2.89 w-6 shrink-0"
-            ></Image>
-            <Link href={`/writer/co-author?columnId=${columnIdState}`} passHref>
-              <div className="h-6.00625 text-3.5 font-400 lh-6 ml-2.07 mt-2.89 w-14 shrink-0">
-                共创作者
-              </div>
-            </Link>
-          </div>
-          <div
-            className={`w-65.25 h-12.4705 flex shrink-0 ${pathname === "/writer/speed-up"
-              ? "bg-[#DAF9F1] text-[#4CC5A6]"
-              : "bg-[#FFF] text-[#999]"
-              }`}
-          >
-            <Image
-              src={`${pathname === "/writer/speed-up"
-                ? "/images/writer/edit/Rocket6 copy.svg"
-                : "/images/writer/edit/Rocket6.svg"
-                }`}
-              alt="Monitor1"
-              width={24}
-              height={24.03}
-              className="h-6.00625 ml-10.555 mt-2.89 w-6 shrink-0"
-            ></Image>
-            <Link href={`/writer/speed-up?columnId=${columnIdState}`} passHref>
-              <div className="h-6.00625 text-3.5 font-400 lh-6 ml-2.07 mt-2.89 w-14 shrink-0">
-                加速计划
-              </div>
-            </Link>
-          </div>
+          {navConfig.slice(1).map(renderNavItem)}
         </div>
       </div>
     </>
