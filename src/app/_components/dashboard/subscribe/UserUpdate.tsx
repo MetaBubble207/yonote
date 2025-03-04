f472e980405fa4d9b6c7ac774cf763f26bfb4dbb
xiaban <980892894@qq.com>
2025-03-04 13:47:08 +0800

    fix: 修复订阅管理改变可视状态时，更新模块的专栏没有及时改变的问题

--- src/app/_components/dashboard/subscribe/UserUpdate.tsx
+++ src/app/_components/dashboard/subscribe/UserUpdate.tsx
@@ -2,21 +2,25 @@ import { api } from "@/trpc/react";
 import NoData from "@/app/_components/common/NoData";
 import { LoadingSkeleton } from "../../common/LoadingSkeleton";
 import SubscribeRenew from "./SubscribeRenew";
+import { useAppSelector } from "@/app/_hooks/useRedux";
+import { userSubscribeSelector } from "@/app/_slice/user-subscribe-slice";
 
 
 export default function UserUpdate({ token }: { token: string | null }) {
+    const { subscribeColumnList } = useAppSelector(userSubscribeSelector)
+
     const { data: updateColumnData, isLoading } = api.column.getUpdateColumn.useQuery(
         token!,
         { enabled: Boolean(token) }  // 只有在有 token 时才发起请求
     );
-
     if (isLoading) return <LoadingSkeleton />;
-    if (!updateColumnData?.length)
+    // 第二个判断条件主要是因为订阅管理的状态改变不好触发该组件更新，因此需要结合 redux 的状态和 updateColumnData 来判断是否显示该组件
+    if (!updateColumnData?.length || updateColumnData?.filter(item => subscribeColumnList.find(sub => sub.id === item.id && sub.isVisible)).length === 0)
         return <NoData title="你订阅的所有专栏都暂未更新噢😁~" />;
 
     return (
         <div>
-            {updateColumnData.map((column) => (
+            {updateColumnData.filter(item => subscribeColumnList.find(sub => sub.id === item.id && sub.isVisible)).map((column) => (
                 <SubscribeRenew key={column.id} column={column} />
             ))}
         </div>
